596ae42c60379f067ef67a7e4e47933eda6c1ddb
fanghao <fanghao11@huawei.com>
2019-01-25 15:02:27 +0800

    wd/zip: add zip stream mode compress/decompress support
    Support zlib/gzip compress/decompress.
    
    Signed-off-by: fanghao <fanghao11@huawei.com>

--- include/zip_usr_if.h
+++ include/zip_usr_if.h
@@ -18,7 +18,7 @@ struct hisi_zip_sqe {
 	__u32 dw12;
 	__u32 tag;
 	__u32 dest_avail_out;
-	__u32 rsvd0;
+	__u32 ctx_dw0;
 	__u32 comp_head_addr_l;
 	__u32 comp_head_addr_h;
 	__u32 source_addr_l;
@@ -31,7 +31,50 @@ struct hisi_zip_sqe {
 	__u32 cipher_key1_addr_h;
 	__u32 cipher_key2_addr_l;
 	__u32 cipher_key2_addr_h;
-	__u32 rsvd1[4];
+	__u32 ctx_dw1;
+	__u32 ctx_dw2;
+	__u32 isize;
+	__u32 checksum;
+
+};
+
+/* Flush types */
+enum wd_comp_flush {
+	WD_INVALID_FLUSH,
+
+	/* output as much data as we can to improve performance */
+	WD_NO_FLUSH,
+
+	/* output as bytes aligning or some other conditions satisfied */
+	WD_SYNC_FLUSH,
+
+	/* indicates the end of the file/data */
+	WD_FINISH,
+};
+
+#define STREAM_FLUSH_SHIFT	25
+
+enum alg_type {
+	HW_ZLIB  = 0x02,
+	HW_GZIP,
+};
+enum hw_comp_op {
+	HW_DEFLATE,
+	HW_INFLATE,
+};
+enum hw_flush {
+	HZ_SYNC_FLUSH,
+	HZ_FINISH,
+};
+
+enum hw_state {
+	STATELESS,
+	STATEFUL,
+};
+
+enum hw_stream_status {
+	STREAM_OLD,
+	STREAM_NEW,
 };
 
 #endif
