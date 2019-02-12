7490b9e9ff95d036fad53336ac8f6309ca716207
fanghao <fanghao11@huawei.com>
2019-02-12 19:18:52 +0800

    zip/test: add gzip stream support for ES HW
    Add length info in gzip protocol extra region.
    
    Signed-off-by: fanghao <fanghao11@huawei.com>
    Signed-off-by: Kenneth Lee <liguozhu@hisilicon.com>

--- test/test_hisi_zip.c
+++ test/test_hisi_zip.c
@@ -32,8 +32,16 @@ do { \
 #define ZLIB_HEADER "\x78\x9c"
 #define ZLIB_HEADER_SZ 2
 
-#define GZIP_HEADER "\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03"
+/*
+ * We use a extra field for gzip block length. So the fourth byte is \x04.
+ * This is necessary because our software don't know the size of block when
+ * using an hardware decompresser (It is known by hardware). This help our
+ * decompresser to work and helpfully, compatible with gzip.
+ */
+#define GZIP_HEADER "\x1f\x8b\x08\x04\x00\x00\x00\x00\x00\x03"
 #define GZIP_HEADER_SZ 10
+#define GZIP_EXTRA_SZ 10
+#define GZIP_TAIL_SZ 8
 
 /* bytes of data for a request */
 static int block_size = 512000;
@@ -80,11 +88,12 @@ static void hizip_wd_sched_init_cache(struct wd_scheduler *sched, int i)
 
 static int hizip_wd_sched_input(struct wd_msg *msg, void *priv)
 {
-	size_t ilen, sz;
+	size_t ilen, templen, real_len, sz;
 	struct hisi_zip_sqe *m = msg->msg;
 
 	ilen = hizip_priv.total_len > block_size ?
 		block_size : hizip_priv.total_len;
+	templen = ilen;
 	hizip_priv.total_len -= ilen;
 	if (hizip_priv.op_type == INFLATE) {
 		if (hizip_priv.alg_type == ZLIB) {
@@ -97,6 +106,19 @@ static int hizip_wd_sched_input(struct wd_msg *msg, void *priv)
 				   hizip_priv.sfile);
 			SYS_ERR_COND(sz != GZIP_HEADER_SZ, "read");
 			ilen -= GZIP_HEADER_SZ;
+			if (*((char *)msg->data_in + 3) == 0x04) {
+				sz = fread(msg->data_in, 1, GZIP_EXTRA_SZ,
+					   hizip_priv.sfile);
+				memcpy(&ilen, msg->data_in + 6, 4);
+				dbg("gzip iuput len %ld\n", ilen);
+				SYS_ERR_COND(ilen > block_size,
+				    "gzip protocol_len(%ld) > block_size(%d)\n",
+				    ilen, block_size);
+				real_len = GZIP_HEADER_SZ
+					+ GZIP_EXTRA_SZ + ilen;
+				hizip_priv.total_len = hizip_priv.total_len
+					+ templen - real_len;
+			}
 		}
 	}
 
@@ -120,12 +142,14 @@ static int hizip_wd_sched_output(struct wd_msg *msg, void *priv)
 	struct hisi_zip_sqe *m = msg->msg;
 	__u32 status = m->dw3 & 0xff;
 	__u32 type = m->dw9 & 0xff;
+	char gzip_extra[GZIP_EXTRA_SZ] = {0x00, 0x07, 0x48, 0x69, 0x00, 0x04,
+					  0x00, 0x00, 0x00, 0x00};
 
-	dbg("zip output(%p, %p): %x, %x, %x, %x, %d, %d\n",
+	dbg("zip output(%p, %p): %x, %x, %x, %x, %d, %d, consume=%d, out=%d\n",
 	    msg, m,
 	    m->source_addr_l, m->source_addr_h,
 	    m->dest_addr_l, m->dest_addr_h,
-	    m->dest_avail_out, m->input_data_length);
+	    m->dest_avail_out, m->input_data_length, m->consumed, m->produced);
 
 	SYS_ERR_COND(status != 0 && status != 0x0d, "bad status (s=%d, t=%d)\n",
 		     status, type);
@@ -139,6 +163,10 @@ static int hizip_wd_sched_output(struct wd_msg *msg, void *priv)
 			sz = fwrite(GZIP_HEADER, 1, GZIP_HEADER_SZ,
 				    hizip_priv.dfile);
 			SYS_ERR_COND(sz != GZIP_HEADER_SZ, "write");
+			memcpy(gzip_extra + 6, &m->produced, 4);
+			sz = fwrite(gzip_extra, 1, GZIP_EXTRA_SZ,
+				    hizip_priv.dfile);
+			SYS_ERR_COND(sz != GZIP_EXTRA_SZ, "write");
 		}
 	}
 	sz = fwrite(msg->data_out, 1, m->produced, hizip_priv.dfile);
@@ -198,7 +226,7 @@ err_with_qs:
 	return ret;
 }
 
-void hizip_fini()
+void hizip_fini(void)
 {
 	wd_sched_fini(&sched);
 	free(hizip_priv.msgs);
@@ -286,7 +314,7 @@ int main(int argc, char *argv[])
 		default:
 			show_help = 1;
 			break;
-               }
+		}
 	}
 
 	SYS_ERR_COND(show_help || optind > argc,
