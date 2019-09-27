51973dec88060833d8fddc4a5c3dec8b3d501a49
Hao Fang <fanghao11@huawei.com>
2019-09-27 15:56:37 +0800

    wd: v1: fix for wd_zip_test -k perfermace test issue
    The wd_sched.c use the wd_recv_sync func, the return -EBUSY may
    not into the poll interface.But modefy return 0 may execute the
    poll wait function, will lead the perf test more slower, which
    changed by DTS:DTS2019090614895 Description:WD:modify error code
    according interface doc.
    
    This fix the wd_sched.c use the wd_recv() instead.
    
    Also other fixes update as wd_alg interface.
    
    Signed-off-by: Hao Fang <fanghao11@huawei.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- v1/wd_comp.h
+++ v1/wd_comp.h
@@ -15,27 +15,27 @@ typedef unsigned long long __u64;
 
 /* now hw not support config */
 enum wcrypto_comp_level {
-	WCRYPTO_COMP_L1 = 1,		/* Compression level 1 */
-	WCRYPTO_COMP_L2,		/* Compression level 2 */
-	WCRYPTO_COMP_L3,		/* Compression level 3 */
-	WCRYPTO_COMP_L4,		/* Compression level 4 */
-	WCRYPTO_COMP_L5,		/* Compression level 5 */
-	WCRYPTO_COMP_L6,		/* Compression level 6 */
-	WCRYPTO_COMP_L7,		/* Compression level 7 */
-	WCRYPTO_COMP_L8,		/* Compression level 8 */
-	WCRYPTO_COMP_L9,		/* Compression level 9 */
+	WCRYPTO_COMP_L1 = 1, /* Compression level 1 */
+	WCRYPTO_COMP_L2,     /* Compression level 2 */
+	WCRYPTO_COMP_L3,     /* Compression level 3 */
+	WCRYPTO_COMP_L4,     /* Compression level 4 */
+	WCRYPTO_COMP_L5,     /* Compression level 5 */
+	WCRYPTO_COMP_L6,     /* Compression level 6 */
+	WCRYPTO_COMP_L7,     /* Compression level 7 */
+	WCRYPTO_COMP_L8,     /* Compression level 8 */
+	WCRYPTO_COMP_L9,     /* Compression level 9 */
 };
 
 /* now hw not support config */
 enum wcrypto_comp_win_type {
-	WCRYPTO_COMP_WS_4K, /* 4k bytes window size */
-	WCRYPTO_COMP_WS_8K, /* 8k bytes window size */
+	WCRYPTO_COMP_WS_4K,  /* 4k bytes window size */
+	WCRYPTO_COMP_WS_8K,  /* 8k bytes window size */
 	WCRYPTO_COMP_WS_16K, /* 16k bytes window size */
 	WCRYPTO_COMP_WS_32K, /* 32k bytes window size */
 };
 
 /* Flush types */
-enum wcrypto_comp_flush {
+enum wcrypto_comp_flush_type {
 	WCRYPTO_INVALID_FLUSH,
 
 	/* output as much data as we can to improve performance */
@@ -80,24 +80,26 @@ enum wcrypto_stream_status {
 
 /**
  * different contexts for different users/threads
+ * @cb: call back functions of user
  * @alg_type:compressing algorithm type zlib/gzip
  * @op_type:operational types deflate/inflate
  * @stream_mode:stateless(block)/statefull
  * @comp_lv: compressing level;maybe support later
  * @win_size: window size of algorithm;maybe support later
- * @cb: call back functions of user
+ * @data_fmt: buffer format
  * @ss_buf:field for user to alloc dma buffer
  * @next_in: next input byte
  * @next_out: next output byte should be put there
  * @ctx_buf: need extra memory for hw
  */
 struct wcrypto_comp_ctx_setup {
+	wcrypto_cb cb;
 	__u8 alg_type;
 	__u8 op_type;
 	__u8 stream_mode;
 	__u8 comp_lv;
-	__u32 win_size;
-	wcrypto_cb cb;
+	__u16 win_size;
+	__u16 data_fmt;
 	void *ss_buf;
 	void *next_in;
 	void *next_out;
@@ -135,31 +137,29 @@ struct wcrypto_comp_op_data {
 };
 
 struct wcrypto_comp_msg {
-	__u32 alg_type;
-	__u32 in_size;
-	__u32 avail_out;
-	__u32 in_cons; /* consumed bytes of input data */
-	__u32 produced; /* produced bytes of current operation */
-	__u8 *src;
-	__u8 *dst;
-	__u32 tag;
-	__u8 comp_lv;
-	__u8 file_type;
-	__u8 humm_type;
-	__u8 op_type;
-	__u32 win_size;
-	/* This flag indicates the output mode, from enum wcrypto_comp_flush */
-	__u8 flush_type;
-	__u8 stream_mode;
-	__u8 stream_pos;
-	__u32 status;
-	__u64 udata;
-	__u32 isize;
-	__u32 checksum;
-	__u32 ctx_priv0;
-	__u32 ctx_priv1;
-	__u32 ctx_priv2;
-	void *ctx_buf;
+	__u8 alg_type;   /* Denoted by enum wcrypto_comp_alg_type */
+	__u8 op_type;    /* Denoted by enum wcrypto_comp_op_type */
+	__u8 flush_type; /* Denoted by enum wcrypto_comp_flush_type */
+	__u8 stream_mode;/* Denoted by enum wcrypto_comp_state */
+	__u8 stream_pos; /* Denoted by enum wcrypto_stream_status */
+	__u8 comp_lv;	 /* Denoted by enum wcrypto_comp_level */
+	__u8 data_fmt;	 /* Data format, denoted by enum wd_buff_type */
+	__u8 win_sz;	 /* Denoted by enum wcrypto_comp_win_type */
+	__u32 in_size;   /* Input data bytes */
+	__u32 avail_out; /* Output buffer size */
+	__u32 in_cons;   /* consumed bytes of input data */
+	__u32 produced;  /* produced bytes of current operation */
+	__u8 *src;       /* Input data VA, buf should be DMA-able. */
+	__u8 *dst;       /* Output data VA pointer */
+	__u32 win_size;  /* Denoted by enum wcrypto_comp_win_type */
+	__u32 status;    /* Denoted by error code and enum wcrypto_op_result */
+	__u32 isize;	 /* Denoted by gzip isize */
+	__u32 checksum;  /* Denoted by zlib/gzip CRC */
+	__u32 ctx_priv0; /* Denoted HW priv */
+	__u32 ctx_priv1; /* Denoted HW priv */
+	__u32 ctx_priv2; /* Denoted HW priv */
+	void *ctx_buf;   /* Denoted HW ctx cache */
+	__u64 udata;     /* Input user tag, indentify data of stream/user */
 };
 
 /**
@@ -167,7 +167,8 @@ struct wcrypto_comp_msg {
  * @q: wrapdrive queue, need requested by user.
  * @setup:setup data of user
  */
-void *wcrypto_create_comp_ctx(struct wd_queue *q, struct wcrypto_comp_ctx_setup *setup);
+void *wcrypto_create_comp_ctx(struct wd_queue *q,
+			      struct wcrypto_comp_ctx_setup *setup);
 
 /**
  * wcrypto_do_comp() - syn/asynchronous compressing/decompressing operation
