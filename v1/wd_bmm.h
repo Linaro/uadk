b2ffd194e33c7514a61724f376e70830d43c3240
Meng Yu <yumeng18@huawei.com>
2019-10-08 16:40:16 +0800

    wd: v1: revoke interface change in BMM
    1. Modify the size in wd_calc_memsize from u32 to ulong.
    2. Add an output parameter of wd_get_free_blk_num.
    
    Signed-off-by: Meng Yu <yumeng18@huawei.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- v1/wd_bmm.h
+++ v1/wd_bmm.h
@@ -16,8 +16,8 @@ extern void *wd_blkpool_create(struct wd_queue *q,
 extern void wd_blkpool_destroy(void *pool);
 extern void *wd_alloc_blk(void *pool);
 extern void wd_free_blk(void *pool, void *blk);
-extern unsigned int wd_get_free_blk_num(void *pool);
-extern unsigned int wd_blk_alloc_failures(void *pool);
+extern int wd_get_free_blk_num(void *pool, __u32 *free_num);
+extern int wd_blk_alloc_failures(void *pool, __u32 *fail_num);
 extern void *wd_blk_iova_map(void *pool, void *blk);
 extern void wd_blk_iova_unmap(void *pool, void *blk_dma, void *blk);
 #endif
