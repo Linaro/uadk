965991cad70679bb3d0c566352cd0dccac30f5f4
Junxian Liu <liujunxian3@huawei.com>
2020-01-07 10:54:38 +0800

    wd: v1: clean code on ZIP
    1. Simplify loops.
    2. Avoid one function exceeding 50 lines.
    
    Signed-off-by: Junxian Liu <liujunxian3@huawei.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- v1/drv/hisi_zip_udrv.c
+++ v1/drv/hisi_zip_udrv.c
@@ -87,37 +87,75 @@ static void qm_fill_zip_sqe_with_priv(struct hisi_zip_sqe *sqe, void *priv)
 	if (!udata)
 		return;
 
-	sqe->lba_l = udata->dif.lba & QM_L32BITS_MASK;
-	sqe->lba_h = udata->dif.lba >> QM_HADDR_SHIFT;
+	sqe->lba_l = lower_32_bits(udata->dif.lba);
+	sqe->lba_h = upper_32_bits(udata->dif.lba);
 	sqe->dw7 = udata->sgl.in_sge_data_off;
 	sqe->dw8 = udata->sgl.out_sge_data_off;
-	sqe->dw9 = udata->req_type | udata->buf_type << HZ_BUF_TYPE_SHIFT |
-		udata->align_size << HZ_ALIGN_SIZE_SHIFT;
-	sqe->dw10 = udata->dif.ctrl.gen.page_layout_gen_type |
-		udata->dif.ctrl.gen.grd_gen_type << HZ_GRD_GTYPE_SHIFT |
-		udata->dif.ctrl.gen.ver_gen_type << HZ_VER_GTYPE_SHIFT |
-		udata->dif.ctrl.gen.app_gen_type << HZ_APP_GTYPE_SHIFT |
-		udata->dif.app << HZ_APP_SHIFT | udata->dif.ver << HZ_VER_SHIFT;
+	sqe->dw9 = (udata->req_type) | (udata->buf_type << HZ_BUF_TYPE_SHIFT) |
+		(udata->align_size << HZ_ALIGN_SIZE_SHIFT);
+	sqe->dw10 = (udata->dif.ctrl.gen.page_layout_gen_type) |
+		(udata->dif.ctrl.gen.grd_gen_type << HZ_GRD_GTYPE_SHIFT) |
+		(udata->dif.ctrl.gen.ver_gen_type << HZ_VER_GTYPE_SHIFT) |
+		(udata->dif.ctrl.gen.app_gen_type << HZ_APP_GTYPE_SHIFT) |
+		(udata->dif.app << HZ_APP_SHIFT) | (udata->dif.ver << HZ_VER_SHIFT);
 	sqe->priv_info = udata->dif.priv_info;
-	sqe->dw12 = udata->dif.ctrl.gen.ref_gen_type |
-		udata->dif.ctrl.gen.page_layout_pad_type << HZ_PAD_TYPE_SHIFT |
-		udata->dif.ctrl.verify.grd_verify_type << HZ_GRD_VTYPE_SHIFT |
-		udata->dif.ctrl.verify.ref_verify_type << HZ_REF_VTYPE_SHIFT |
-		udata->block_size << HZ_BLK_SIZE_SHIFT;
-	sqe->comp_head_addr_l = (uintptr_t)udata->comp_head & QM_L32BITS_MASK;
-	sqe->comp_head_addr_h = (uintptr_t)udata->comp_head >> QM_HADDR_SHIFT;
+	sqe->dw12 = (udata->dif.ctrl.gen.ref_gen_type) |
+		(udata->dif.ctrl.gen.page_layout_pad_type << HZ_PAD_TYPE_SHIFT) |
+		(udata->dif.ctrl.verify.grd_verify_type << HZ_GRD_VTYPE_SHIFT) |
+		(udata->dif.ctrl.verify.ref_verify_type << HZ_REF_VTYPE_SHIFT) |
+		(udata->block_size << HZ_BLK_SIZE_SHIFT);
+	sqe->comp_head_addr_l = lower_32_bits((uintptr_t)udata->comp_head);
+	sqe->comp_head_addr_h = upper_32_bits((uintptr_t)udata->comp_head);
 }
 
+static int qm_fill_zip_sqe_get_phy_addr(struct hisi_zip_sqe *sqe,
+		struct wcrypto_comp_msg *msg, struct wd_queue *q)
+{
+	struct q_info *qinfo = q->qinfo;
+	uintptr_t phy_in, phy_out;
+	uintptr_t phy_ctxbuf = 0;
+
+	if (qinfo->dev_flags & UACCE_DEV_NOIOMMU) {
+		phy_in = (uintptr_t)drv_iova_map(q, msg->src, msg->in_size);
+		if (!phy_in) {
+			WD_ERR("Get zip in buf dma address fail!\n");
+			return -WD_ENOMEM;
+		}
+		phy_out = (uintptr_t)drv_iova_map(q, msg->dst, msg->avail_out);
+		if (!phy_out) {
+			WD_ERR("Get zip out buf dma address fail!\n");
+			return -WD_ENOMEM;
+		}
+		if (msg->stream_mode == WCRYPTO_COMP_STATEFUL) {
+			phy_ctxbuf = (uintptr_t)drv_iova_map(q, msg->ctx_buf, 0);
+			if (!phy_ctxbuf) {
+				WD_ERR("Get zip ctx buf dma address fail!\n");
+				return -WD_ENOMEM;
+			}
+		}
+	} else {
+		phy_in = (uintptr_t)msg->src;
+		phy_out = (uintptr_t)msg->dst;
+		if (msg->stream_mode == WCRYPTO_COMP_STATEFUL)
+			phy_ctxbuf = (uintptr_t)msg->ctx_buf;
+	}
+	sqe->source_addr_l = lower_32_bits((__u64)phy_in);
+	sqe->source_addr_h = upper_32_bits((__u64)phy_in);
+	sqe->dest_addr_l = lower_32_bits((__u64)phy_out);
+	sqe->dest_addr_h = upper_32_bits((__u64)phy_out);
+	sqe->stream_ctx_addr_l = lower_32_bits((__u64)phy_ctxbuf);
+	sqe->stream_ctx_addr_h = upper_32_bits((__u64)phy_ctxbuf);
+
+	return WD_SUCCESS;
+}
 
 int qm_fill_zip_sqe(void *smsg, struct qm_queue_info *info, __u16 i)
 {
 	struct hisi_zip_sqe *sqe = (struct hisi_zip_sqe *)info->sq_base + i;
 	struct wcrypto_comp_msg *msg = smsg;
 	struct wcrypto_comp_tag *tag = (void *)(uintptr_t)msg->udata;
-	uintptr_t phy_in, phy_out;
-	uintptr_t phy_ctxbuf = 0;
 	struct wd_queue *q = info->q;
-	struct q_info *qinfo = q->qinfo;
+	int ret;
 
 	memset((void *)sqe, 0, sizeof(*sqe));
 
@@ -132,48 +170,20 @@ int qm_fill_zip_sqe(void *smsg, struct qm_queue_info *info, __u16 i)
 		return -WD_EINVAL;
 	}
 
-	if (qinfo->dev_flags & UACCE_DEV_NOIOMMU) {
-		phy_in = (uintptr_t)drv_iova_map(q, msg->src, msg->in_size);
-		if (!phy_in) {
-			WD_ERR("Get zip in buf dma address fail!\n");
-			return -WD_ENOMEM;
-		}
-		phy_out = (uintptr_t)drv_iova_map(q, msg->dst, 0);
-		if (!phy_out) {
-			WD_ERR("Get zip out buf dma address fail!\n");
-			return -WD_ENOMEM;
-		}
-		if (msg->stream_mode == WCRYPTO_COMP_STATEFUL) {
-			phy_ctxbuf = (uintptr_t)drv_iova_map(q,
-				msg->ctx_buf, 0);
-			if (!phy_ctxbuf) {
-				WD_ERR("Get zip ctx buf dma address fail!\n");
-				return -WD_ENOMEM;
-			}
-		}
-	} else {
-		phy_in = (uintptr_t)msg->src;
-		phy_out = (uintptr_t)msg->dst;
-		if (msg->stream_mode == WCRYPTO_COMP_STATEFUL)
-			phy_ctxbuf = (uintptr_t)msg->ctx_buf;
-	}
+	ret = qm_fill_zip_sqe_get_phy_addr(sqe, msg, q);
+	if (ret)
+		return ret;
 
 	msg->flush_type = (msg->flush_type == WCRYPTO_FINISH) ? HZ_FINISH :
 			  HZ_SYNC_FLUSH;
-	sqe->dw7 |= ((msg->stream_pos << STREAM_POS_SHIFT |
-		     msg->stream_mode << STREAM_MODE_SHIFT |
-		     msg->flush_type)) << STREAM_FLUSH_SHIFT;
-	sqe->source_addr_l = (__u64)phy_in & QM_L32BITS_MASK;
-	sqe->source_addr_h = (__u64)phy_in >> QM_HADDR_SHIFT;
-	sqe->dest_addr_l = (__u64)phy_out & QM_L32BITS_MASK;
-	sqe->dest_addr_h = (__u64)phy_out >> QM_HADDR_SHIFT;
+	sqe->dw7 |= ((msg->stream_pos << STREAM_POS_SHIFT) |
+		     (msg->stream_mode << STREAM_MODE_SHIFT) |
+		     (msg->flush_type)) << STREAM_FLUSH_SHIFT;
 	sqe->input_data_length = msg->in_size;
 	if (msg->avail_out > MIN_AVAILOUT_SIZE)
 		sqe->dest_avail_out = msg->avail_out;
 	else
 		sqe->dest_avail_out = MIN_AVAILOUT_SIZE;
-	sqe->stream_ctx_addr_l = (__u64)phy_ctxbuf & QM_L32BITS_MASK;
-	sqe->stream_ctx_addr_h = (__u64)phy_ctxbuf >> QM_HADDR_SHIFT;
 	sqe->ctx_dw0 = msg->ctx_priv0;
 	sqe->ctx_dw1 = msg->ctx_priv1;
 	sqe->ctx_dw2 = msg->ctx_priv2;
@@ -192,6 +202,30 @@ int qm_fill_zip_sqe(void *smsg, struct qm_queue_info *info, __u16 i)
 #endif
 
 	return WD_SUCCESS;
+}
+
+static void qm_parse_zip_sqe_set_status(struct wcrypto_comp_msg *recv_msg,
+				__u32 status, __u16 lstblk, __u16 ctx_st)
+{
+	if ((status == HW_DECOMP_END) && lstblk)
+		recv_msg->status = WCRYPTO_DECOMP_END;
+	else if (status == HW_CRC_ERR) /* deflate type no crc, do normal */
+		recv_msg->status = WD_VERIFY_ERR;
+	else if (status == HW_IN_DATA_DIF_CHECK_ERR)
+		recv_msg->status = WCRYPTO_SRC_DIF_ERR;
+	else if (status == HW_UNCOMP_DIF_CHECK_ERR)
+		recv_msg->status = WCRYPTO_DST_DIF_ERR;
+	else if (status == HW_NEGACOMPRESS)
+		recv_msg->status = WCRYPTO_NEGTIVE_COMP_ERR;
+
+	/* deflate type no crc, need return status */
+	if (ctx_st == HW_DECOMP_NO_CRC)
+		recv_msg->status = WCRYPTO_DECOMP_NO_CRC;
+	/* last block no space, need resend null size req */
+	else if (ctx_st == HW_DECOMP_NO_SPACE)
+		recv_msg->status = WCRYPTO_DECOMP_END_NOSPACE;
+	else if (ctx_st == HW_DECOMP_BLK_NOSTART)
+		recv_msg->status = WCRYPTO_DECOMP_BLK_NOSTART;
 }
 
 int qm_parse_zip_sqe(void *hw_msg, const struct qm_queue_info *info,
@@ -232,25 +266,7 @@ int qm_parse_zip_sqe(void *hw_msg, const struct qm_queue_info *info,
 	recv_msg->checksum = sqe->checksum;
 	recv_msg->tag = sqe->tag;
 
-	if ((status == HW_DECOMP_END) && lstblk)
-		recv_msg->status = WCRYPTO_DECOMP_END;
-	else if (status == HW_CRC_ERR) /* deflate type no crc, do normal*/
-		recv_msg->status = WD_VERIFY_ERR;
-	else if (status == HW_IN_DATA_DIF_CHECK_ERR)
-		recv_msg->status = WCRYPTO_SRC_DIF_ERR;
-	else if (status == HW_UNCOMP_DIF_CHECK_ERR)
-		recv_msg->status = WCRYPTO_DST_DIF_ERR;
-	else if (status == HW_NEGACOMPRESS)
-		recv_msg->status = WCRYPTO_NEGTIVE_COMP_ERR;
-
-	/* deflate type no crc, need return status */
-	if (ctx_st == HW_DECOMP_NO_CRC)
-		recv_msg->status = WCRYPTO_DECOMP_NO_CRC;
-	/* last block no space, need resend null size req */
-	else if (ctx_st == HW_DECOMP_NO_SPACE)
-		recv_msg->status = WCRYPTO_DECOMP_END_NOSPACE;
-	else if (ctx_st == HW_DECOMP_BLK_NOSTART)
-		recv_msg->status = WCRYPTO_DECOMP_BLK_NOSTART;
+	qm_parse_zip_sqe_set_status(recv_msg, status, lstblk, ctx_st);
 
 	dbg("%s: %p, %p, %d\n", __func__, info->req_cache[i], sqe,
 	    info->sqe_size);
