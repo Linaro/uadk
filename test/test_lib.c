ef37a0cd9dc585f56495416924535003a650ec9b
Jean-Philippe Brucker <jean-philippe@linaro.org>
2020-02-25 15:19:05 +0000

    test: Move more code to test_lib.c
    A lot of the code in test_perf would be duplicated when adding a new
    test. Move it to test_lib.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- test/test_lib.c
+++ test/test_lib.c
@@ -1,5 +1,261 @@
+#include <signal.h>
+
 #include "test_lib.h"
 #include "drv/hisi_qm_udrv.h"
+
+struct check_rand_ctx {
+	int off;
+	unsigned long global_off;
+	__u32 last;
+	unsigned short state[3];
+};
+
+#ifdef DEBUG_LOG
+static void dbg_sqe(const char *head, struct hisi_zip_sqe *m)
+{
+	fprintf(stderr,
+		"=== %s ===\n"
+		"cons=0x%x prod=0x%x in=0x%x out=0x%x comp=0x%x\n"
+		"lba=0x%08x 0x%08x\n"
+		"dw3=0x%x dw7=0x%x dw8=0x%x dw9=0x%x dw10=0x%x dw12=0x%x\n"
+		"priv=0x%x tag=0x%x\n"
+		"ctx dw0=0x%x dw1=0x%x dw2=0x%x\n"
+		"comp head addr=0x%08x 0x%08x\n"
+		"source addr=0x%08x 0x%08x\n"
+		"dest addr=0x%08x 0x%08x\n"
+		"stream ctx addr=0x%08x 0x%08x\n"
+		"cipher key1 addr=0x%08x 0x%08x\n"
+		"cipher key2 addr=0x%08x 0x%08x\n"
+		"isize=0x%x checksum=0x%x\n"
+		"=== === === ===\n",
+
+		head,
+		m->consumed, m->produced, m->input_data_length,
+		m->dest_avail_out, m->comp_data_length,
+		m->lba_h, m->lba_l,
+		m->dw3, m->dw7, m->dw8, m->dw9, m->dw10, m->dw12,
+		m->priv_info, m->tag,
+		m->ctx_dw0, m->ctx_dw1, m->ctx_dw2,
+		m->comp_head_addr_h, m->comp_head_addr_l,
+		m->source_addr_h, m->source_addr_l,
+		m->dest_addr_h, m->dest_addr_l,
+		m->stream_ctx_addr_h, m->stream_ctx_addr_l,
+		m->cipher_key1_addr_h, m->cipher_key1_addr_l,
+		m->cipher_key2_addr_h, m->cipher_key2_addr_l,
+		m->isize, m->checksum
+	       );
+}
+#else
+#define dbg_sqe(...)
+#endif
+
+static void hizip_wd_sched_init_cache(struct wd_scheduler *sched, int i)
+{
+	struct wd_msg *wd_msg = &sched->msgs[i];
+	struct hisi_zip_sqe *msg;
+	struct hizip_test_context *ctx = sched->priv;
+
+	msg = wd_msg->msg = &ctx->msgs[i];
+
+	if (ctx->opts->alg_type == ZLIB)
+		msg->dw9 = HW_ZLIB;
+	else
+		msg->dw9 = HW_GZIP;
+	msg->dest_avail_out = sched->msg_data_size;
+
+	/*
+	 * This test doesn't use the data_in and data_out prepared by
+	 * wd_sched_init. Instead we ask the zip device to directly work on our
+	 * buffers to avoid a memcpy.
+	 * TODO: don't alloc buffers
+	 */
+
+	if (!(ctx->flags & UACCE_DEV_SVA)) {
+		void *data_in, *data_out;
+
+		data_in = wd_get_pa_from_va(&sched->qs[0], wd_msg->data_in);
+		data_out = wd_get_pa_from_va(&sched->qs[0], wd_msg->data_out);
+
+		msg->source_addr_l = (__u64)data_in & 0xffffffff;
+		msg->source_addr_h = (__u64)data_in >> 32;
+		msg->dest_addr_l = (__u64)data_out & 0xffffffff;
+		msg->dest_addr_h = (__u64)data_out >> 32;
+	}
+}
+
+static int hizip_wd_sched_input(struct wd_msg *msg, void *priv)
+{
+	size_t ilen;
+	char *in_buf, *out_buf;
+	struct hisi_zip_sqe *m = msg->msg;
+	struct hizip_test_context *ctx = priv;
+	struct test_options *opts = ctx->opts;
+
+	ilen = ctx->total_len > opts->block_size ?
+		opts->block_size : ctx->total_len;
+
+	in_buf = ctx->in_buf;
+	out_buf = ctx->out_buf;
+
+	if (!(ctx->flags & UACCE_DEV_SVA)) {
+		memcpy(msg->data_in, in_buf, ilen);
+	} else {
+		msg->data_in = in_buf;
+		msg->data_out = out_buf;
+
+		m->source_addr_l = (__u64)in_buf & 0xffffffff;
+		m->source_addr_h = (__u64)in_buf >> 32;
+		m->dest_addr_l = (__u64)out_buf & 0xffffffff;
+		m->dest_addr_h = (__u64)out_buf >> 32;
+
+		ctx->out_buf += ilen * EXPANSION_RATIO;
+	}
+
+	m->input_data_length = ilen;
+	ctx->in_buf += ilen;
+	ctx->total_len -= ilen;
+
+	dbg_sqe("zip input", m);
+
+	return 0;
+}
+
+static int hizip_wd_sched_output(struct wd_msg *msg, void *priv)
+{
+	struct hizip_test_context *ctx = priv;
+	struct hisi_zip_sqe *m = msg->msg;
+	__u32 status = m->dw3 & 0xff;
+	__u32 type = m->dw9 & 0xff;
+
+	if (ctx->opts->faults & INJECT_SIG_WORK)
+		kill(0, SIGTERM);
+
+	dbg_sqe("zip output", m);
+
+	SYS_ERR_COND(status != 0 && status != 0x0d, "bad status (s=%d, t=%d)\n",
+		     status, type);
+
+	ctx->total_out += m->produced;
+	return 0;
+}
+
+struct test_ops test_ops = {
+	.init_cache = hizip_wd_sched_init_cache,
+	.input = hizip_wd_sched_input,
+	.output = hizip_wd_sched_output,
+};
+
+void hizip_prepare_random_input_data(struct hizip_test_context *ctx)
+{
+	__u32 seed = 0;
+	unsigned short rand_state[3] = {(seed >> 16) & 0xffff, seed & 0xffff, 0x330e};
+
+	unsigned long remain_size;
+	__u32 block_size, size;
+	char *in_buf;
+	size_t i, j;
+
+	/*
+	 * TODO: change state for each buffer, to make sure there is no TLB
+	 * aliasing. Can we store the seed into priv_info?
+	 */
+	//__u32 seed = ctx->state++;
+	block_size = ctx->opts->block_size;
+	remain_size = ctx->total_len;
+	in_buf = ctx->in_buf;
+
+	while (remain_size > 0) {
+		if (remain_size > block_size)
+			size = block_size;
+		else
+			size = remain_size;
+		/*
+		 * Prepare the input buffer with a reproducible sequence of
+		 * numbers. nrand48() returns a pseudo-random number in the
+		 * interval [0; 2^31). It's not really possible to compress a
+		 * pseudo-random stream using deflate, since it can't find any
+		 * string repetition. As a result the output size is bigger,
+		 * with a ratio of 1.041.
+		 */
+		for (i = 0; i < size; i += 4) {
+			__u64 n = nrand48(rand_state);
+
+			for (j = 0; j < 4 && i + j < size; j++)
+				in_buf[i + j] = (n >> (8 * j)) & 0xff;
+		}
+
+		in_buf += size;
+		remain_size -= size;
+	}
+}
+
+static int hizip_check_rand(unsigned char *buf, unsigned int size, void *opaque)
+{
+	int i;
+	int *j;
+	__u32 n;
+	struct check_rand_ctx *rand_ctx = opaque;
+
+	j = &rand_ctx->off;
+	for (i = 0; i < size; i += 4) {
+		if (*j) {
+			/* Somthing left from a previous run */
+			n = rand_ctx->last;
+		} else {
+			n = nrand48(rand_ctx->state);
+			rand_ctx->last = n;
+		}
+		for (; *j < 4 && i + *j < size; (*j)++) {
+			char expected = (n >> (8 * *j)) & 0xff;
+			char actual = buf[i + *j];
+
+			if (expected != actual) {
+				WD_ERR("Invalid decompressed char at offset %lu: expected 0x%x != 0x%x\n",
+				       rand_ctx->global_off + i + *j, expected,
+				       actual);
+				return -EINVAL;
+			}
+		}
+		if (*j == 4)
+			*j = 0;
+	}
+	rand_ctx->global_off += size;
+	return 0;
+}
+
+int hizip_verify_random_output(char *out_buf, struct test_options *opts,
+			       struct hizip_test_context *ctx)
+{
+	int ret;
+	int seed = 0;
+	off_t off = 0;
+	size_t checked = 0;
+	size_t total_checked = 0;
+	struct check_rand_ctx rand_ctx = {
+		.state = {(seed >> 16) & 0xffff, seed & 0xffff, 0x330e},
+	};
+
+	if (!opts->verify)
+		return 0;
+
+	do {
+		ret = hizip_check_output(out_buf + off, ctx->total_out,
+					 &checked, hizip_check_rand, &rand_ctx);
+		if (ret) {
+			WD_ERR("Check output failed with %d\n", ret);
+			return ret;
+		}
+		total_checked += checked;
+		off += opts->block_size * EXPANSION_RATIO;
+	} while (!ret && total_checked < opts->total_len);
+
+	if (rand_ctx.global_off != opts->total_len) {
+		WD_ERR("Invalid output size %lu != %lu\n",
+		       rand_ctx.global_off, opts->total_len);
+		return -EINVAL;
+	}
+	return 0;
+}
 
 /*
  * Initialize the scheduler with the given options and operations.
@@ -48,6 +304,23 @@ int hizip_test_init(struct wd_scheduler *sched, struct test_options *opts,
 
 err_with_qs:
 	free(sched->qs);
+	return ret;
+}
+
+int hizip_test_sched(struct wd_scheduler *sched, struct test_options *opts,
+		     struct hizip_test_context *ctx)
+{
+	int ret = 0;
+
+	while (ctx->total_len || !wd_sched_empty(sched)) {
+		dbg("request loop: total_len=%d\n", ctx->total_len);
+		ret = wd_sched_work(sched, ctx->total_len);
+		if (ret < 0) {
+			WD_ERR("wd_sched_work: %d\n", ret);
+			break;
+		}
+	}
+
 	return ret;
 }
 
--- test/test_sva_perf.c
+++ test/test_sva_perf.c
@@ -62,298 +62,6 @@ struct hizip_stats {
 	double v[NUM_STATS];
 };
 
-struct hizip_priv {
-	struct test_options *opts;
-	char *in_buf;
-	char *out_buf;
-	unsigned long total_len;
-	struct hisi_zip_sqe *msgs;
-	int flags;
-	size_t total_out;
-};
-
-struct check_rand_ctx {
-	int off;
-	unsigned long global_off;
-	__u32 last;
-	unsigned short state[3];
-};
-
-static void hizip_wd_sched_init_cache(struct wd_scheduler *sched, int i)
-{
-	struct wd_msg *wd_msg = &sched->msgs[i];
-	struct hisi_zip_sqe *msg;
-	struct hizip_priv *priv = sched->priv;
-
-	msg = wd_msg->msg = &priv->msgs[i];
-
-	if (priv->opts->alg_type == ZLIB)
-		msg->dw9 = HW_ZLIB;
-	else
-		msg->dw9 = HW_GZIP;
-	msg->dest_avail_out = sched->msg_data_size;
-
-	/*
-	 * This test doesn't use the data_in and data_out prepared by
-	 * wd_sched_init. Instead we ask the zip device to directly work on our
-	 * buffers to avoid a memcpy.
-	 * TODO: don't alloc buffers
-	 */
-
-	if (!(priv->flags & UACCE_DEV_SVA)) {
-		void *data_in, *data_out;
-
-		data_in = wd_get_pa_from_va(&sched->qs[0], wd_msg->data_in);
-		data_out = wd_get_pa_from_va(&sched->qs[0], wd_msg->data_out);
-
-		msg->source_addr_l = (__u64)data_in & 0xffffffff;
-		msg->source_addr_h = (__u64)data_in >> 32;
-		msg->dest_addr_l = (__u64)data_out & 0xffffffff;
-		msg->dest_addr_h = (__u64)data_out >> 32;
-	}
-}
-
-#ifdef DEBUG_LOG
-static void dbg_sqe(const char *head, struct hisi_zip_sqe *m)
-{
-	fprintf(stderr,
-		"=== %s ===\n"
-		"cons=0x%x prod=0x%x in=0x%x out=0x%x comp=0x%x\n"
-		"lba=0x%08x 0x%08x\n"
-		"dw3=0x%x dw7=0x%x dw8=0x%x dw9=0x%x dw10=0x%x dw12=0x%x\n"
-		"priv=0x%x tag=0x%x\n"
-		"ctx dw0=0x%x dw1=0x%x dw2=0x%x\n"
-		"comp head addr=0x%08x 0x%08x\n"
-		"source addr=0x%08x 0x%08x\n"
-		"dest addr=0x%08x 0x%08x\n"
-		"stream ctx addr=0x%08x 0x%08x\n"
-		"cipher key1 addr=0x%08x 0x%08x\n"
-		"cipher key2 addr=0x%08x 0x%08x\n"
-		"isize=0x%x checksum=0x%x\n"
-		"=== === === ===\n",
-
-		head,
-		m->consumed, m->produced, m->input_data_length,
-		m->dest_avail_out, m->comp_data_length,
-		m->lba_h, m->lba_l,
-		m->dw3, m->dw7, m->dw8, m->dw9, m->dw10, m->dw12,
-		m->priv_info, m->tag,
-		m->ctx_dw0, m->ctx_dw1, m->ctx_dw2,
-		m->comp_head_addr_h, m->comp_head_addr_l,
-		m->source_addr_h, m->source_addr_l,
-		m->dest_addr_h, m->dest_addr_l,
-		m->stream_ctx_addr_h, m->stream_ctx_addr_l,
-		m->cipher_key1_addr_h, m->cipher_key1_addr_l,
-		m->cipher_key2_addr_h, m->cipher_key2_addr_l,
-		m->isize, m->checksum
-	       );
-}
-#else
-#define dbg_sqe(...)
-#endif
-
-static int hizip_wd_sched_input(struct wd_msg *msg, void *priv)
-{
-	size_t ilen;
-	char *in_buf, *out_buf;
-	struct hisi_zip_sqe *m = msg->msg;
-	struct hizip_priv *hizip_priv = priv;
-	struct test_options *opts = hizip_priv->opts;
-
-	ilen = hizip_priv->total_len > opts->block_size ?
-		opts->block_size : hizip_priv->total_len;
-
-	in_buf = hizip_priv->in_buf;
-	out_buf = hizip_priv->out_buf;
-
-	if (!(hizip_priv->flags & UACCE_DEV_SVA)) {
-		memcpy(msg->data_in, in_buf, ilen);
-	} else {
-		msg->data_in = in_buf;
-		msg->data_out = out_buf;
-
-		m->source_addr_l = (__u64)in_buf & 0xffffffff;
-		m->source_addr_h = (__u64)in_buf >> 32;
-		m->dest_addr_l = (__u64)out_buf & 0xffffffff;
-		m->dest_addr_h = (__u64)out_buf >> 32;
-
-		hizip_priv->out_buf += ilen * EXPANSION_RATIO;
-	}
-
-	m->input_data_length = ilen;
-	hizip_priv->in_buf += ilen;
-	hizip_priv->total_len -= ilen;
-
-	dbg_sqe("zip input", m);
-
-	return 0;
-}
-
-static int hizip_wd_sched_output(struct wd_msg *msg, void *priv)
-{
-	struct hizip_priv *hizip_priv = priv;
-	struct test_options *opts = hizip_priv->opts;
-	struct hisi_zip_sqe *m = msg->msg;
-	__u32 status = m->dw3 & 0xff;
-	__u32 type = m->dw9 & 0xff;
-
-	if (opts->option & PERFORMANCE) {
-		if (!(hizip_priv->flags & UACCE_DEV_SVA)) {
-			/* for performance test, simulate mmecpy in non-sva mode */
-			memcpy(hizip_priv->out_buf, msg->data_out, m->produced);
-			hizip_priv->out_buf += opts->block_size * EXPANSION_RATIO;
-		}
-	}
-
-	dbg_sqe("zip output", m);
-
-	SYS_ERR_COND(status != 0 && status != 0x0d, "bad status (s=%d, t=%d)\n",
-		     status, type);
-
-	hizip_priv->total_out += m->produced;
-	return 0;
-}
-
-static struct test_ops test_ops = {
-	.init_cache = hizip_wd_sched_init_cache,
-	.input = hizip_wd_sched_input,
-	.output = hizip_wd_sched_output,
-};
-
-static void hizip_prepare_input_data(struct hizip_priv *hizip_priv)
-{
-	__u32 seed = 0;
-	unsigned short rand_state[3] = {(seed >> 16) & 0xffff, seed & 0xffff, 0x330e};
-
-	unsigned long remain_size;
-	__u32 block_size, size;
-	char *in_buf;
-	size_t i, j;
-
-	/*
-	 * TODO: change state for each buffer, to make sure there is no TLB
-	 * aliasing. Can we store the seed into priv_info?
-	 */
-	//__u32 seed = hizip_priv->state++;
-	block_size = hizip_priv->opts->block_size;
-	remain_size = hizip_priv->total_len;
-	in_buf = hizip_priv->in_buf;
-
-	while (remain_size > 0) {
-		if (remain_size > block_size)
-			size = block_size;
-		else
-			size = remain_size;
-		/*
-		 * Prepare the input buffer with a reproducible sequence of
-		 * numbers. nrand48() returns a pseudo-random number in the
-		 * interval [0; 2^31). It's not really possible to compress a
-		 * pseudo-random stream using deflate, since it can't find any
-		 * string repetition. As a result the output size is bigger,
-		 * with a ratio of 1.041.
-		 */
-		for (i = 0; i < size; i += 4) {
-			__u64 n = nrand48(rand_state);
-
-			for (j = 0; j < 4 && i + j < size; j++)
-				in_buf[i + j] = (n >> (8 * j)) & 0xff;
-		}
-
-		in_buf += size;
-		remain_size -= size;
-	}
-}
-
-static int hizip_test_sched(struct wd_scheduler *sched,
-			    struct test_options *opts, struct hizip_priv *priv)
-{
-	int ret = 0;
-
-	if (opts->option & TEST_ZLIB)
-		return zlib_deflate(priv->out_buf, priv->total_len *
-				    EXPANSION_RATIO, priv->in_buf,
-				    priv->total_len, &priv->total_out);
-
-	while (priv->total_len || !wd_sched_empty(sched)) {
-		dbg("request loop: total_len=%d\n", priv->total_len);
-		ret = wd_sched_work(sched, priv->total_len);
-		if (ret < 0) {
-			WD_ERR("wd_sched_work: %d\n", ret);
-			break;
-		}
-	}
-
-	return ret;
-}
-
-static int hizip_check_rand(unsigned char *buf, unsigned int size, void *opaque)
-{
-	int i;
-	int *j;
-	__u32 n;
-	struct check_rand_ctx *rand_ctx = opaque;
-
-	j = &rand_ctx->off;
-	for (i = 0; i < size; i += 4) {
-		if (*j) {
-			/* Somthing left from a previous run */
-			n = rand_ctx->last;
-		} else {
-			n = nrand48(rand_ctx->state);
-			rand_ctx->last = n;
-		}
-		for (; *j < 4 && i + *j < size; (*j)++) {
-			char expected = (n >> (8 * *j)) & 0xff;
-			char actual = buf[i + *j];
-
-			if (expected != actual) {
-				WD_ERR("Invalid decompressed char at offset %lu: expected 0x%x != 0x%x\n",
-				       rand_ctx->global_off + i + *j, expected,
-				       actual);
-				return -EINVAL;
-			}
-		}
-		if (*j == 4)
-			*j = 0;
-	}
-	rand_ctx->global_off += size;
-	return 0;
-}
-
-static int hizip_verify_output(char *out_buf, struct test_options *opts,
-			       struct hizip_priv *priv)
-{
-	int ret;
-	int seed = 0;
-	off_t off = 0;
-	size_t checked = 0;
-	size_t total_checked = 0;
-	struct check_rand_ctx rand_ctx = {
-		.state = {(seed >> 16) & 0xffff, seed & 0xffff, 0x330e},
-	};
-
-	if (!opts->verify)
-		return 0;
-
-	do {
-		ret = hizip_check_output(out_buf + off, priv->total_out,
-					 &checked, hizip_check_rand, &rand_ctx);
-		if (ret) {
-			WD_ERR("Check output failed with %d\n", ret);
-			return ret;
-		}
-		total_checked += checked;
-		off += opts->block_size * EXPANSION_RATIO;
-	} while (!ret && total_checked < opts->total_len);
-
-	if (rand_ctx.global_off != opts->total_len) {
-		WD_ERR("Invalid output size %lu != %lu\n",
-		       rand_ctx.global_off, opts->total_len);
-		return -EINVAL;
-	}
-	return 0;
-}
-
 static int perf_event_open(struct perf_event_attr *attr,
 			   pid_t pid, int cpu, int group_fd,
 			   unsigned long flags)
@@ -493,7 +201,7 @@ static int run_one_test(struct test_options *opts, struct hizip_stats *stats)
 	int *perf_fds;
 	void *in_buf, *out_buf;
 	struct wd_scheduler sched = {0};
-	struct hizip_priv hizip_priv = {0}, hizip_save;
+	struct hizip_test_context ctx = {0}, ctx_save;
 	struct timespec setup_time, start_time, end_time;
 	struct timespec setup_cputime, start_cputime, end_cputime;
 	struct rusage setup_rusage, start_rusage, end_rusage;
@@ -502,27 +210,26 @@ static int run_one_test(struct test_options *opts, struct hizip_stats *stats)
 	stats->v[ST_SEND] = stats->v[ST_RECV] = stats->v[ST_SEND_RETRY] =
 			    stats->v[ST_RECV_RETRY] = 0;
 
-	hizip_priv.opts = opts;
-	hizip_priv.msgs = calloc(opts->req_cache_num, sizeof(*hizip_priv.msgs));
-	if (!hizip_priv.msgs)
+	ctx.opts = opts;
+	ctx.msgs = calloc(opts->req_cache_num, sizeof(*ctx.msgs));
+	if (!ctx.msgs)
 		return -ENOMEM;
 
-	hizip_priv.total_len = opts->total_len;
+	ctx.total_len = opts->total_len;
 
-	in_buf = hizip_priv.in_buf = mmap_alloc(hizip_priv.total_len);
+	in_buf = ctx.in_buf = mmap_alloc(ctx.total_len);
 	if (!in_buf) {
 		ret = -ENOMEM;
 		goto out_with_msgs;
 	}
 
-	out_buf = hizip_priv.out_buf = mmap_alloc(hizip_priv.total_len *
-						  EXPANSION_RATIO);
+	out_buf = ctx.out_buf = mmap_alloc(ctx.total_len * EXPANSION_RATIO);
 	if (!out_buf) {
 		ret = -ENOMEM;
 		goto out_with_in_buf;
 	}
 
-	hizip_prepare_input_data(&hizip_priv);
+	hizip_prepare_random_input_data(&ctx);
 
 	perf_event_get("iommu/dev_fault", &perf_fds, &nr_fds);
 
@@ -537,27 +244,27 @@ static int run_one_test(struct test_options *opts, struct hizip_stats *stats)
 		 * Enhance performance in sva case
 		 * no impact to non-sva case
 		 */
-		memset(out_buf, 0, hizip_priv.total_len * EXPANSION_RATIO);
+		memset(out_buf, 0, ctx.total_len * EXPANSION_RATIO);
 	}
 
-	ret = hizip_test_init(&sched, opts, &test_ops, &hizip_priv);
+	ret = hizip_test_init(&sched, opts, &test_ops, &ctx);
 	if (ret) {
 		WD_ERR("hizip init fail with %d\n", ret);
 		goto out_with_out_buf;
 	}
 	if (sched.qs)
-		hizip_priv.flags = sched.qs[0].dev_flags;
+		ctx.flags = sched.qs[0].dev_flags;
 
-	hizip_save = hizip_priv;
+	ctx_save = ctx;
 
 	clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_cputime);
 	getrusage(RUSAGE_SELF, &start_rusage);
 
 	for (j = 0; j < opts->compact_run_num; j++) {
-		hizip_priv = hizip_save;
+		ctx = ctx_save;
 
-		ret = hizip_test_sched(&sched, opts, &hizip_priv);
+		ret = hizip_test_sched(&sched, opts, &ctx);
 		if (ret < 0) {
 			WD_ERR("hizip test fail with %d\n", ret);
 			goto out_with_fini;
@@ -600,7 +307,7 @@ static int run_one_test(struct test_options *opts, struct hizip_stats *stats)
 
 	/* check last loop is enough, same as below hizip_verify_output */
 	stats->v[ST_COMPRESSION_RATIO] = (double)opts->total_len /
-					 hizip_priv.total_out * 100;
+					 ctx.total_out * 100;
 
 	stats->v[ST_SPEED] = opts->compact_run_num * opts->total_len /
 		(stats->v[ST_RUN_TIME] / 1000) / 1024 / 1024 * 1000 * 1000;
@@ -615,16 +322,16 @@ static int run_one_test(struct test_options *opts, struct hizip_stats *stats)
 	stats->v[ST_CPU_IDLE] = (v - stats->v[ST_CPU_TIME]) / v * 100;
 	stats->v[ST_FAULTS] = stats->v[ST_MAJFLT] + stats->v[ST_MINFLT];
 
-	ret = hizip_verify_output(out_buf, opts, &hizip_priv);
+	ret = hizip_verify_random_output(out_buf, opts, &ctx);
 
 out_with_fini:
 	hizip_test_fini(&sched, opts);
 out_with_out_buf:
-	munmap(out_buf, hizip_priv.total_len);
+	munmap(out_buf, ctx.total_len * EXPANSION_RATIO);
 out_with_in_buf:
-	munmap(in_buf, hizip_priv.total_len * EXPANSION_RATIO);
+	munmap(in_buf, ctx.total_len);
 out_with_msgs:
-	free(hizip_priv.msgs);
+	free(ctx.msgs);
 	return ret;
 }
 
@@ -926,11 +633,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	/*
-	 * Align size to the next block. We allow non-power-of-two block sizes.
-	 */
-	opts.total_len = (opts.total_len + opts.block_size - 1) /
-			 opts.block_size * opts.block_size;
+	hizip_test_adjust_len(&opts);
 
 	SYS_ERR_COND(show_help || optind > argc,
 		     "test_bind_api [opts]\n"
