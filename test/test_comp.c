8c876d86acbde35cb202543fec79889765f4fbf8
Haojian Zhuang <haojian.zhuang@linaro.org>
2020-03-06 09:17:00 +0000

    test: support decompress in test_comp
    Support decompress in test_comp. Change the command argument to support
    it.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- test/test_comp.c
+++ test/test_comp.c
@@ -11,6 +11,10 @@
 
 #define PATHLEN		256
 
+#define FLAG_ZLIB	(1 << 0)
+#define FLAG_GZIP	(1 << 1)
+#define FLAG_DECMPS	(1 << 2)
+
 static int verify_mask(wd_dev_mask_t *mask, int id)
 {
 	int	offs;
@@ -94,15 +98,20 @@ static int test_accel_mask(void)
 	return 0;
 }
 
-int test_compress(char *src, char *dst)
+int test_compress(char *src, char *dst, int flag)
 {
 	handler_t		handle;
 	struct wd_comp_arg	arg;
 	struct stat		st;
 	int	sfd, dfd, ret;
+	char	algs[60];
 
 	if (!src || !dst)
 		return -EINVAL;
+	if (flag & FLAG_ZLIB)
+		sprintf(algs, "zlib");
+	else if (flag & FLAG_GZIP)
+		sprintf(algs, "gzip");
 	sfd = open(src, O_RDONLY);
 	if (sfd < 0)
 		return -EINVAL;
@@ -114,22 +123,33 @@ int test_compress(char *src, char *dst)
 	memset(&arg, 0, sizeof(struct wd_comp_arg));
 	fstat(sfd, &st);
 	arg.src_len = st.st_size;
-	arg.src = calloc(1, sizeof(char) * arg.src_len);
+	arg.src = malloc(sizeof(char) * arg.src_len);
 	if (!arg.src) {
 		ret = -ENOMEM;
 		goto out_src;
 	}
-	arg.dst_len = arg.src_len + (arg.src_len >> 1);
-	arg.dst = calloc(1, sizeof(char) * arg.dst_len);
+	arg.dst_len = arg.src_len << 2;	// for decompress
+	arg.dst = malloc(sizeof(char) * arg.dst_len);
 	if (!arg.dst) {
 		ret = -ENOMEM;
 		goto out_dst;
 	}
 	ret = read(sfd, arg.src, arg.src_len);
-	handle = wd_alg_comp_alloc_sess("gzip", NULL);
-	ret = wd_alg_compress(handle, &arg);
-	if (ret) {
-		fprintf(stderr, "fail to compress (%d)\n", ret);
+	if ((ret < 0) || (ret != arg.src_len)) {
+		fprintf(stderr, "fail to load data (%d)\n", ret);
+		goto out_read;
+	}
+	handle = wd_alg_comp_alloc_sess(algs, NULL);
+	if (flag & FLAG_DECMPS) {
+		ret = wd_alg_decompress(handle, &arg);
+		if (ret) {
+			fprintf(stderr, "fail to decompress (%d)\n", ret);
+		}
+	} else {
+		ret = wd_alg_compress(handle, &arg);
+		if (ret) {
+			fprintf(stderr, "fail to compress (%d)\n", ret);
+		}
 	}
 	wd_alg_comp_free_sess(handle);
 	ret = write(dfd, arg.dst, arg.dst_len);
@@ -138,6 +158,8 @@ int test_compress(char *src, char *dst)
 	close(dfd);
 	close(sfd);
 	return 0;
+out_read:
+	free(arg.dst);
 out_dst:
 	free(arg.src);
 out_src:
@@ -150,11 +172,11 @@ out:
 int main(int argc, char *argv[])
 {
 	int	fd, opt;
-	int	ret;
+	int	ret, flag = 0, flag_mask;
 	wd_dev_mask_t		*dev_mask;
 	char	src[PATHLEN+1], dst[PATHLEN+1];
 
-	while ((opt = getopt(argc, argv, "i:o:h")) != -1) {
+	while ((opt = getopt(argc, argv, "i:o:hdgz")) != -1) {
 		switch (opt) {
 		case 'i':
 			snprintf(src, PATHLEN, "%s", optarg);
@@ -162,8 +184,17 @@ int main(int argc, char *argv[])
 		case 'o':
 			snprintf(dst, PATHLEN, "%s", optarg);
 			break;
+		case 'd':
+			flag |= FLAG_DECMPS;
+			break;
+		case 'g':
+			flag |= FLAG_GZIP;
+			break;
+		case 'z':
+			flag |= FLAG_ZLIB;
+			break;
 		case 'h':
-			printf("./a.out -i <src file> -o <dst file>\n");
+			printf("./test_comp -i <src> -o <dst> [-d] [-z] [-g]\n");
 			break;
 		default:
 			fprintf(stderr, "Unrecognized option!\n");
@@ -175,15 +206,20 @@ int main(int argc, char *argv[])
 		printf("failed to open dev node:%d\n", errno);
 		return fd;
 	}
+	flag_mask = FLAG_GZIP | FLAG_ZLIB;
+	if (!flag || (flag & flag_mask == flag_mask)) {
+		printf("wrong flag setting:%d\n", flag);
+		return -EINVAL;
+	}
 
 /*
 	ret = test_mask();
 	if (ret < 0)
 		printf("failed to pass mask test\n");
-*/
 	test_list();
 	test_accel_mask();
-	test_compress(src, dst);
+*/
+	test_compress(src, dst, flag);
 	close(fd);
 	return 0;
 }
