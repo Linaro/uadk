5a7e096a2fa530f12b3a89f2b2eead7455902eca
Hao Fang <fanghao11@huawei.com>
2020-03-26 21:18:57 +0800

    wd: v1: remove libz
    Signed-off-by: Hao Fang <fanghao11@huawei.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- v1/test/hisi_zip_test/wd_zip_test.c
+++ v1/test/hisi_zip_test/wd_zip_test.c
@@ -23,7 +23,6 @@
 #include <sched.h>
 #include <sys/time.h>
 #include <sys/syscall.h>
-#include "../lib/zlib.h"
 
 #include "../../wd.h"
 #include "zip_alg.h"
@@ -75,188 +74,6 @@ pid_t gettid(void)
 {
 	return syscall(__NR_gettid);
 }
-/* fix for memory buffer compress test.
- * def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
- * allocated for processing, Z_STREAM_ERROR if an invalid compression
- * level is supplied, Z_VERSION_ERROR if the version of zlib.h and
- * the version of the library linked do not match, or Z_ERRNO if
- * there is an error reading or writing the files.
- *
- */
-int def(int level, int type, unsigned char *dst, ulong *dstlen,
-	     unsigned char *src, ulong srclen)
-{
-	int ret, flush;
-	unsigned int have;
-	z_stream strm;
-	unsigned char in[block_size * 2];
-	unsigned char out[block_size * 2];
-	int windowBits = 15;
-	int GZIP_ENCODING = 16;
-
-	*dstlen = 0;
-	/* allocate deflate state */
-	strm.zalloc = Z_NULL;
-	strm.zfree = Z_NULL;
-	strm.opaque = Z_NULL;
-	if (!type) {
-		ret = deflateInit(&strm, level);
-	} else {
-		/* deflate for gzip data */
-		ret = deflateInit2(&strm,
-			      Z_DEFAULT_COMPRESSION,
-			      Z_DEFLATED,
-			      windowBits | GZIP_ENCODING,
-			      8,
-			      Z_DEFAULT_STRATEGY);
-		if (ret != Z_OK)
-			return ret;
-	}
-
-	do {
-		if (srclen > block_size) {
-			memcpy(in, src, block_size);
-			src += block_size;
-			strm.avail_in = block_size;
-			srclen -= block_size;
-		} else {
-			memcpy(in, src, srclen);
-			src += srclen;
-			strm.avail_in = srclen;
-			srclen = 0;
-		}
-		flush = srclen ? Z_NO_FLUSH : Z_FINISH;
-		strm.next_in = in;
-
-		/* run deflate() on input until output buffer
-		 * not full, finish compression if all of source
-		 * has been read in
-		 */
-		do {
-			strm.avail_out = block_size;
-			strm.next_out = out;
-			ret = deflate(&strm, flush);
-			ASSERT(ret != Z_STREAM_ERROR);
-			have = block_size - strm.avail_out;
-			memcpy(dst, out, have);
-			dst += have;
-			*dstlen += have;
-		} while (strm.avail_out == 0);
-
-		ASSERT(strm.avail_in == 0);
-
-	/* done when last data in file processed */
-	} while (flush != Z_FINISH);
-
-	ASSERT(ret == Z_STREAM_END); /* stream will be complete */
-
-	/* clean up and return */
-	(void)deflateEnd(&strm);
-	return Z_OK;
-}
-
-/* fix for memory buffer decompress test.
- * inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be
- * allocated for processing, Z_DATA_ERROR if the deflate data is
- * invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
- * the version of the library linked do not match, or Z_ERRNO if
- * there is an error reading or writing the files.
- */
-int inf(int type, unsigned char *dst, ulong *dstlen,
-	     unsigned char *src, ulong srclen)
-{
-	int ret;
-	unsigned int have;
-	z_stream strm;
-	unsigned char in[block_size * 2];
-	unsigned char out[block_size * 2];
-
-	*dstlen = 0;
-	/* allocate inflate state */
-	strm.zalloc = Z_NULL;
-	strm.zfree = Z_NULL;
-	strm.opaque = Z_NULL;
-	strm.avail_in = 0;
-	strm.next_in = Z_NULL;
-
-	if (!type) {
-		ret = inflateInit(&strm);
-	} else {
-		ret = inflateInit2(&strm, 16 + MAX_WBITS);
-		if (ret != Z_OK)
-			return ret;
-	}
-	do {
-		if (srclen > block_size) {
-			memcpy(in, src, block_size);
-			src += block_size;
-			strm.avail_in = block_size;
-			srclen -= block_size;
-		} else {
-			memcpy(in, src, srclen);
-			src += srclen;
-			strm.avail_in = srclen;
-			srclen = 0;
-		}
-
-		if (strm.avail_in == 0)
-			break;
-		strm.next_in = in;
-
-		/* run inflate() on input until output buffer not
-		 * full
-		 */
-		do {
-			strm.avail_out = block_size;
-			strm.next_out = out;
-			ret = inflate(&strm, Z_NO_FLUSH);
-			ASSERT(ret != Z_STREAM_ERROR);
-			switch (ret) {
-			case Z_NEED_DICT:
-				ret = Z_DATA_ERROR;/* and fall through */
-			case Z_DATA_ERROR:
-			case Z_MEM_ERROR:
-				(void)inflateEnd(&strm);
-				return ret;
-			}
-			have = block_size - strm.avail_out;
-			memcpy(dst, out, have);
-			dst += have;
-			*dstlen += have;
-		} while (strm.avail_out == 0);
-
-		/* done when inflate() says it's done */
-	} while (ret != Z_STREAM_END);
-
-	/* clean up and return */
-	(void)inflateEnd(&strm);
-	return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-}
-
-/* report a zlib or i/o error */
-void zerr(int ret)
-{
-	fputs("zpipe: ", stderr);
-	switch (ret) {
-	case Z_ERRNO:
-		if (ferror(stdin))
-			fputs("error reading stdin\n", stderr);
-		if (ferror(stdout))
-			fputs("error writing stdout\n", stderr);
-		break;
-	case Z_STREAM_ERROR:
-		fputs("invalid compression level\n", stderr);
-		break;
-	case Z_DATA_ERROR:
-		fputs("invalid or incomplete deflate data\n", stderr);
-		break;
-	case Z_MEM_ERROR:
-		fputs("out of memory\n", stderr);
-		break;
-	case Z_VERSION_ERROR:
-		fputs("zlib version mismatch!\n", stderr);
-	}
-}
 
 /* stream mode test thread */
 void *zlib_sys_stream_test_thread(void *args)
@@ -299,16 +116,7 @@ therad_no_affinity:
 				if (ret < 0)
 					WD_ERR("comp fail! id=%d tid=%d ret=%d\n",
 						cpu_id, (int)tid, ret);
-			} else {
-/* ret = compress(pdata->dst, &pdata->dst_len, pdata->src, pdata->src_len); */
-				ret = def(Z_DEFAULT_COMPRESSION,
-					  pdata->alg_type,
-					  pdata->dst, &pdata->dst_len,
-					  pdata->src, pdata->src_len);
-				if (ret < 0)
-					WD_ERR("sw comp fail! id=%d tid=%d ret=%d\n",
-						cpu_id, (int)tid, ret);
-			}
+			} 		
 		} else if (pdata->op_type == WCRYPTO_INFLATE) {
 			if (pdata->hw_flag) {
 				ret = hw_stream_decompress(pdata->alg_type,
@@ -320,16 +128,9 @@ therad_no_affinity:
 				if (ret < 0)
 					WD_ERR("decomp fail! id=%d tid=%d ret=%d\n",
 						cpu_id, (int)tid, ret);
-			} else {
-/* ret = uncompress(pdata->dst, &pdata->dst_len, pdata->src, pdata->src_len); */
-				ret = inf(pdata->alg_type,
-					  pdata->dst, &pdata->dst_len,
-					  pdata->src, pdata->src_len);
-				if (ret < 0)
-					WD_ERR("sw decomp fail! id=%d tid=%d\n",
-						cpu_id, (int)tid);
 			}
 		}
+
 		if (verify) {
 			ret = hw_stream_decompress(pdata->alg_type,
 						   pdata->blksize,
