e218060c39f6c9cb53740c6af565394ef0312801
Haojian Zhuang <haojian.zhuang@linaro.org>
2020-06-03 14:25:08 +0100

    test/example: add new case for stream interface
    1. Add new single buffer test case to verify stream interface.
    2. Change the meaning of arg->src_len.
       Before deflation/inflation, arg->src_len means the data size of IN buffer.
       After deflation/inflation, arg->src_len means the consumed data size of
       IN buffer.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- test/example.c
+++ test/example.c
@@ -338,7 +338,6 @@ int test_rand_buffer(int flag, int mode)
 			wd_arg.src_len = 0;
 		} else
 			wd_arg.src_len = templen;
-		i += templen;
 		ret = wd_alg_compress(handle, &wd_arg);
 		if (ret < 0)
 			goto out_comp;
@@ -346,11 +345,12 @@ int test_rand_buffer(int flag, int mode)
 			dst_idx += wd_arg.dst_len;
 		if (i <= TEST_LARGE_BUF_LEN) {
 			/* load src with LARGE_BUF_SIZE */
-			if (wd_arg.status & STATUS_IN_EMPTY)
+			if (wd_arg.status & STATUS_IN_EMPTY) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-			else if (wd_arg.status & STATUS_IN_PART_USE) {
+			} else if (wd_arg.status & STATUS_IN_PART_USE) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-				i -= wd_arg.src_len;
 			}
 		}
 		if ((wd_arg.status & STATUS_OUT_DRAINED) &&
@@ -383,7 +383,6 @@ int test_rand_buffer(int flag, int mode)
 			wd_arg.src_len = 0;
 		} else
 			wd_arg.src_len = templen;
-		i += templen;
 		ret = wd_alg_decompress(handle, &wd_arg);
 		if (ret < 0)
 			goto out_comp;
@@ -391,11 +390,12 @@ int test_rand_buffer(int flag, int mode)
 			dst_idx += wd_arg.dst_len;
 		if (i <= len) {
 			/* load src with LARGE_BUF_SIZE */
-			if (wd_arg.status & STATUS_IN_EMPTY)
+			if (wd_arg.status & STATUS_IN_EMPTY) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-			else if (wd_arg.status & STATUS_IN_PART_USE) {
+			} else if (wd_arg.status & STATUS_IN_PART_USE) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-				i -= wd_arg.src_len;
 			}
 		}
 		if ((wd_arg.status & STATUS_OUT_DRAINED) &&
@@ -492,7 +492,6 @@ int test_large_buffer(int flag, int mode)
 			wd_arg.src_len = 0;
 		} else
 			wd_arg.src_len = templen;
-		i += templen;
 		ret = wd_alg_compress(handle, &wd_arg);
 		if (ret < 0)
 			goto out_comp;
@@ -500,11 +499,12 @@ int test_large_buffer(int flag, int mode)
 			dst_idx += wd_arg.dst_len;
 		if (i <= TEST_LARGE_BUF_LEN) {
 			/* load src with LARGE_BUF_SIZE */
-			if (wd_arg.status & STATUS_IN_EMPTY)
+			if (wd_arg.status & STATUS_IN_EMPTY) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-			else if (wd_arg.status & STATUS_IN_PART_USE) {
+			} else if (wd_arg.status & STATUS_IN_PART_USE) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-				i -= wd_arg.src_len;
 			}
 		}
 		if ((wd_arg.status & STATUS_OUT_DRAINED) &&
@@ -537,7 +537,6 @@ int test_large_buffer(int flag, int mode)
 			wd_arg.src_len = 0;
 		} else
 			wd_arg.src_len = templen;
-		i += templen;
 		ret = wd_alg_decompress(handle, &wd_arg);
 		if (ret < 0)
 			goto out_comp;
@@ -545,11 +544,12 @@ int test_large_buffer(int flag, int mode)
 			dst_idx += wd_arg.dst_len;
 		if (i <= len) {
 			/* load src with LARGE_BUF_SIZE */
-			if (wd_arg.status & STATUS_IN_EMPTY)
+			if (wd_arg.status & STATUS_IN_EMPTY) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-			else if (wd_arg.status & STATUS_IN_PART_USE) {
+			} else if (wd_arg.status & STATUS_IN_PART_USE) {
+				i += wd_arg.src_len;
 				templen = LARGE_BUF_SIZE;
-				i -= wd_arg.src_len;
 			}
 		}
 		if ((wd_arg.status & STATUS_OUT_DRAINED) &&
@@ -578,6 +578,367 @@ int test_large_buffer(int flag, int mode)
 	printf("Pass large buffer case for %s algorithm %s\n",
 		(flag == FLAG_ZLIB) ? "zlib" : "gzip", sbuf);
 	wd_alg_comp_free_sess(handle);
+	free(src);
+	free(dst);
+	free(buf);
+	return 0;
+out_comp:
+	wd_alg_comp_free_sess(handle);
+	free(src);
+out_src:
+	free(dst);
+out:
+	free(buf);
+	return ret;
+}
+
+int test_strm_comp_once(int flag)
+{
+	handle_t	handle;
+	struct wd_comp_strm	wd_strm;
+	char	algs[60];
+	char	buf[TEST_WORD_LEN];
+	int	ret = 0, t;
+	void	*src, *dst;
+
+	if (flag & FLAG_ZLIB)
+		sprintf(algs, "zlib");
+	else if (flag & FLAG_GZIP)
+		sprintf(algs, "gzip");
+	memset(&wd_strm, 0, sizeof(struct wd_comp_strm));
+	wd_strm.out_sz = sizeof(char) * TEST_WORD_LEN;
+	wd_strm.in = malloc(sizeof(char) * TEST_WORD_LEN);
+	if (!wd_strm.in) {
+		ret = -ENOMEM;
+		goto out;
+	}
+	/*
+	 * wd_strm.in & wd_strm.in_sz & wd_strm.out_sz will be updated by
+	 * wd_alg_strm_compress() and wd_alg_strm_decompress().
+	 */
+	src = wd_strm.in;
+	wd_strm.out = malloc(sizeof(char) * TEST_WORD_LEN);
+	if (!wd_strm.out) {
+		ret = -ENOMEM;
+		goto out_dst;
+	}
+	dst = wd_strm.out;
+	memcpy(wd_strm.in, word, sizeof(char) * strlen(word));
+	wd_strm.in_sz = strlen(word);
+	wd_strm.out_sz = TEST_WORD_LEN;
+	t = 0;
+	handle = wd_alg_comp_alloc_sess(algs, MODE_STREAM, NULL);
+
+	memset(&wd_strm.arg, 0, sizeof(struct wd_comp_arg));
+	wd_strm.arg.flag = FLAG_INPUT_FINISH;
+	ret = wd_alg_strm_compress(handle, &wd_strm);
+	if (ret < 0)
+		goto out_comp;
+	if (wd_strm.out_sz == 0) {
+		printf("%s: out buffer is full.", __func__);
+		ret = -ENOMEM;
+		goto out_comp;
+	}
+
+	memcpy(buf, dst, wd_strm.total_out);
+	t = wd_strm.total_out;
+	wd_alg_comp_free_sess(handle);
+
+	/* prepare to decompress */
+	memset(&wd_strm, 0, sizeof(struct wd_comp_strm));
+	wd_strm.in = src;
+	wd_strm.in_sz = t;
+	memcpy(wd_strm.in, buf, t);
+	wd_strm.out = dst;
+	wd_strm.out_sz = TEST_WORD_LEN;
+	handle = wd_alg_comp_alloc_sess(algs, MODE_STREAM, NULL);
+
+	memset(&wd_strm.arg, 0, sizeof(struct wd_comp_arg));
+	wd_strm.arg.flag = FLAG_INPUT_FINISH;
+	ret = wd_alg_strm_decompress(handle, &wd_strm);
+	if (ret < 0)
+		goto out_comp;
+	if (wd_strm.out_sz == 0) {
+		printf("%s: out buffer is full.", __func__);
+		ret = -ENOMEM;
+		goto out_comp;
+	}
+
+	wd_alg_comp_free_sess(handle);
+
+	if (memcmp(dst, word, strlen(word))) {
+		printf("match failure! word:%s, buf:%s\n", word, (char *)dst);
+	} else {
+		printf("Pass strm compress test in single buffer\n");
+	}
+	free(src);
+	free(dst);
+	return 0;
+out_comp:
+	wd_alg_comp_free_sess(handle);
+	free(dst);
+out_dst:
+	free(src);
+out:
+	return ret;
+}
+
+/*
+ * Both IN and OUT buffer are 1-byte long.
+ * Compress and decompress on IN and OUT buffer.
+ */
+int test_strm_small_buffer(int flag)
+{
+	handle_t	handle;
+	struct wd_comp_strm	wd_strm;
+	char	algs[60];
+	int	ret = 0, i, len, templen;
+	void	*src, *dst;
+
+	if (flag & FLAG_ZLIB)
+		sprintf(algs, "zlib");
+	else if (flag & FLAG_GZIP)
+		sprintf(algs, "gzip");
+	memset(&wd_strm, 0, sizeof(struct wd_comp_strm));
+	wd_strm.out_sz = sizeof(char) * TEST_WORD_LEN;
+	wd_strm.in = malloc(sizeof(char) * TEST_WORD_LEN);
+	if (!wd_strm.in) {
+		ret = -ENOMEM;
+		goto out;
+	}
+	src = wd_strm.in;
+	wd_strm.out = malloc(sizeof(char) * TEST_WORD_LEN);
+	if (!wd_strm.out) {
+		ret = -ENOMEM;
+		goto out_dst;
+	}
+	dst = wd_strm.out;
+	memcpy(wd_strm.in, word, sizeof(char) * strlen(word));
+	wd_strm.in_sz = 1;
+	wd_strm.out_sz = 1;
+	templen = 1;
+	len = strlen(word);
+	i = 0;
+	handle = wd_alg_comp_alloc_sess(algs, MODE_STREAM, NULL);
+	while (1) {
+		memset(&wd_strm.arg, 0, sizeof(struct wd_comp_arg));
+		wd_strm.in_sz = templen;
+		if (i == len - 1)
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+		else if (i == len) {
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+			wd_strm.in_sz = 0;
+		}
+		ret = wd_alg_strm_compress(handle, &wd_strm);
+		if (ret < 0)
+			goto out_comp;
+		if (wd_strm.out_sz == 0)
+			wd_strm.out_sz = 1;
+		if (i < len) {
+			templen = 1;
+			if (wd_strm.arg.status & STATUS_IN_EMPTY)
+				i++;
+		} else
+			templen = 0;
+		if ((wd_strm.arg.status & STATUS_OUT_DRAINED) &&
+		    (wd_strm.arg.status & STATUS_IN_EMPTY) &&
+		    (wd_strm.arg.flag & FLAG_INPUT_FINISH))
+			break;
+	}
+	wd_alg_comp_free_sess(handle);
+
+
+	/* prepare for decompress */
+	len = wd_strm.total_out;
+	memcpy(src, dst, len);
+	memset(&wd_strm, 0, sizeof(struct wd_comp_strm));
+	templen = 1;
+	wd_strm.in = src;
+	wd_strm.out = dst;
+	wd_strm.in_sz = 1;
+	wd_strm.out_sz = 1;
+	i = 0;
+
+	handle = wd_alg_comp_alloc_sess(algs, MODE_STREAM, NULL);
+	while (1) {
+		memset(&wd_strm.arg, 0, sizeof(struct wd_comp_arg));
+		wd_strm.in_sz = templen;
+		if (i == len - 1)
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+		else if (i == len) {
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+			wd_strm.in_sz = 0;
+		}
+		ret = wd_alg_strm_decompress(handle, &wd_strm);
+		if (ret < 0)
+			goto out_comp;
+		if (wd_strm.out_sz == 0)
+			wd_strm.out_sz = 1;
+		if (i < len) {
+			templen = 1;
+			if (wd_strm.arg.status & STATUS_IN_EMPTY)
+				i++;
+		} else
+			templen = 0;
+		if ((wd_strm.arg.status & STATUS_OUT_DRAINED) &&
+		    (wd_strm.arg.status & STATUS_IN_EMPTY) &&
+		    (wd_strm.arg.flag & FLAG_INPUT_FINISH))
+			break;
+	}
+
+	if (strncmp(word, dst, strlen(word))) {
+		printf("fail to match, dst:%s, word:%s\n", (char *)dst, word);
+		ret = -EFAULT;
+		goto out_comp;
+	} else {
+		printf("Pass small buffer strm case for %s algorithms.\n",
+			(flag == FLAG_ZLIB) ? "zlib" : "gzip");
+	}
+	wd_alg_comp_free_sess(handle);
+	free(src);
+	free(dst);
+	return 0;
+out_comp:
+	wd_alg_comp_free_sess(handle);
+	free(dst);
+out_dst:
+	free(src);
+out:
+	return ret;
+}
+
+int test_strm_rand_buffer(int flag)
+{
+	handle_t	handle;
+	struct wd_comp_strm	wd_strm;
+	char	algs[60];
+	char	*buf, *dst;
+	int	ret = 0, i, len = 0;
+	int	templen;
+	int	ratio = 4;	// inflation needs larger buffer
+	uint64_t	val;
+	uint32_t	seed = 0;
+	unsigned short rand_state[3] = {(seed >> 16) & 0xffff, seed & 0xffff, 0x330e};
+	void	*src;
+
+	buf = malloc(sizeof(char) * TEST_LARGE_BUF_LEN * ratio + LARGE_BUF_SIZE);
+	if (!buf)
+		return -ENOMEM;
+	dst = malloc(sizeof(char) * TEST_LARGE_BUF_LEN * ratio + LARGE_BUF_SIZE);
+	if (!dst) {
+		ret = -ENOMEM;
+		goto out;
+	}
+	if (flag & FLAG_ZLIB)
+		sprintf(algs, "zlib");
+	else if (flag & FLAG_GZIP)
+		sprintf(algs, "gzip");
+	memset(&wd_strm, 0, sizeof(struct wd_comp_strm));
+	src = malloc(sizeof(char) * TEST_LARGE_BUF_LEN + LARGE_BUF_SIZE);
+	if (!src) {
+		ret = -ENOMEM;
+		goto out_src;
+	}
+	// TEST_LARGE_BUF_LEN / 8
+	for (i = 0; i < TEST_LARGE_BUF_LEN >> 3; i++) {
+		val = nrand48(rand_state);
+		*((uint64_t *)src + i) = val;
+	}
+	i = 0;
+	len = TEST_LARGE_BUF_LEN;
+	templen = LARGE_BUF_SIZE;
+	wd_strm.in = src;
+	wd_strm.in_sz = LARGE_BUF_SIZE;
+	wd_strm.out = buf;
+	wd_strm.out_sz = sizeof(char) * TEST_LARGE_BUF_LEN;
+	handle = wd_alg_comp_alloc_sess(algs, MODE_STREAM, NULL);
+	while (1) {
+		memset(&wd_strm.arg, 0, sizeof(struct wd_comp_arg));
+		wd_strm.out_sz = LARGE_BUF_SIZE * ratio;
+		if (i + templen >= len) {
+			templen = len - i;
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+		} else if (i >= len) {
+			templen = 0;
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+		}
+		wd_strm.in_sz = templen;
+		ret = wd_alg_strm_compress(handle, &wd_strm);
+		if (ret < 0)
+			goto out_comp;
+		if (i <= len) {
+			/* load src with LARGE_BUF_SIZE */
+			if (wd_strm.arg.status & STATUS_IN_EMPTY)
+				i += wd_strm.in_sz;
+			else if (wd_strm.arg.status & STATUS_IN_PART_USE)
+				i += wd_strm.in_sz;
+			templen = LARGE_BUF_SIZE;
+		} else
+			templen = 0;
+		if ((wd_strm.arg.status & STATUS_OUT_DRAINED) &&
+		    (wd_strm.arg.status & STATUS_IN_EMPTY) &&
+		    (wd_strm.arg.flag & FLAG_INPUT_FINISH))
+			break;
+	}
+	wd_alg_comp_free_sess(handle);
+
+	/* prepare for decompress */
+	len = wd_strm.total_out;
+	templen = LARGE_BUF_SIZE;
+	memset(&wd_strm, 0, sizeof(struct wd_comp_strm));
+	i = 0;
+	wd_strm.in = buf;
+	wd_strm.in_sz = templen;
+	wd_strm.out = dst;
+	wd_strm.out_sz = sizeof(char) * TEST_LARGE_BUF_LEN;
+	handle = wd_alg_comp_alloc_sess(algs, MODE_STREAM, NULL);
+	while (1) {
+		memset(&wd_strm.arg, 0, sizeof(struct wd_comp_arg));
+		wd_strm.out_sz = LARGE_BUF_SIZE * ratio;
+		if (i + templen >= len) {
+			templen = len - i;
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+		} else if (i >= len) {
+			templen = 0;
+			wd_strm.arg.flag |= FLAG_INPUT_FINISH;
+		}
+		wd_strm.in_sz = templen;
+		ret = wd_alg_strm_decompress(handle, &wd_strm);
+		if (ret < 0)
+			goto out_comp;
+		if (i <= len) {
+			/*
+			 * Always load src with LARGE_BUF_SIZE, templen could
+			 * be updated before decompress.
+			 */
+			if (wd_strm.arg.status & STATUS_IN_EMPTY)
+				i += wd_strm.in_sz;
+			else if (wd_strm.arg.status & STATUS_IN_PART_USE)
+				i += wd_strm.in_sz;
+			templen = LARGE_BUF_SIZE;
+		} else
+			templen = 0;
+		if ((wd_strm.arg.status & STATUS_OUT_DRAINED) &&
+		    (wd_strm.arg.status & STATUS_IN_EMPTY) &&
+		    (wd_strm.arg.flag & FLAG_INPUT_FINISH))
+			break;
+	}
+	wd_alg_comp_free_sess(handle);
+
+	if (wd_strm.total_out != TEST_LARGE_BUF_LEN) {
+		printf("failed on dst size:%ld, expected size:%d\n",
+			wd_strm.total_out, TEST_LARGE_BUF_LEN);
+		goto out_comp;
+	}
+	for (i = 0; i < TEST_LARGE_BUF_LEN; i += LARGE_BUF_SIZE) {
+		ret = memcmp(src + i, dst + i, LARGE_BUF_SIZE);
+		if (ret) {
+			printf("fail to match in %s at %d\n", __func__, i);
+			goto out_comp;
+		}
+	}
+	printf("Pass strm rand buffer case for %s algorithm.\n",
+		(flag == FLAG_ZLIB) ? "zlib" : "gzip");
 	free(src);
 	free(dst);
 	free(buf);
@@ -706,6 +1067,12 @@ int main(int argc, char **argv)
 #endif
 	test_large_buffer(FLAG_ZLIB, MODE_STREAM);
 	test_large_buffer(FLAG_GZIP, MODE_STREAM);
+	test_strm_comp_once(FLAG_ZLIB);
+	test_strm_comp_once(FLAG_GZIP);
+	test_strm_small_buffer(FLAG_ZLIB);
+	test_strm_small_buffer(FLAG_GZIP);
+	test_strm_rand_buffer(FLAG_ZLIB);
+	test_strm_rand_buffer(FLAG_GZIP);
 	thread_fail = 0;
 	test_concurrent(FLAG_ZLIB);
 	if (thread_fail)
