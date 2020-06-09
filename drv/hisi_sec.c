72cec1e926a91e8eca8a466d34efe35f66d367e8
Zhou Wang <wangzhou1@hisilicon.com>
2020-06-09 20:25:30 +0800

    wd: add blank function in hisi_sec driver
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- drv/hisi_sec.c
+++ drv/hisi_sec.c
@@ -1,1 +1,77 @@
 /* SPDX-License-Identifier: Apache-2.0 */
+#include "hisi_sec.h"
+
+struct hisi_comp_sess {
+	handle_t		h_ctx;
+};
+
+int hisi_cipher_init(struct wd_cipher_sess *sess)
+{
+	return 0;
+}
+
+void hisi_cipher_exit(struct wd_cipher_sess *sess)
+{
+}
+
+int hisi_cipher_prep(struct wd_cipher_sess *sess, struct wd_cipher_arg *arg)
+{
+	return 0;
+}
+
+void hisi_cipher_fini(struct wd_cipher_sess *sess)
+{
+}
+
+int hisi_cipher_set_key(struct wd_cipher_sess *sess, const __u8 *key, __u32 key_len)
+{
+	return 0;
+}
+
+int hisi_cipher_encrypt(struct wd_cipher_sess *sess, struct wd_cipher_arg *arg)
+{
+	return 0;
+}
+
+int hisi_cipher_decrypt(struct wd_cipher_sess *sess, struct wd_cipher_arg *arg)
+{
+	return 0;
+}
+
+int hisi_cipher_poll(struct wd_cipher_sess *sess, struct wd_cipher_arg *arg)
+{
+	return 0;
+}
+
+int hisi_digest_init(struct wd_digest_sess *sess)
+{
+	return 0;
+}
+
+void hisi_digest_exit(struct wd_digest_sess *sess)
+{
+}
+
+int hisi_digest_prep(struct wd_digest_sess *sess, struct wd_digest_arg *arg)
+{
+	return 0;
+}
+
+void hisi_digest_fini(struct wd_digest_sess *sess)
+{
+}
+
+int hisi_digest_set_key(struct wd_digest_sess *sess, const __u8 *key, __u32 key_len)
+{
+	return 0;
+}
+
+int hisi_digest_digest(struct wd_digest_sess *sess, struct wd_digest_arg *arg)
+{
+	return 0;
+}
+
+int hisi_digest_poll(struct wd_digest_sess *sess, struct wd_digest_arg *arg)
+{
+	return 0;
+}
