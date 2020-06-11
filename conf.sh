879d3673e289968277fcc4bd4efaf26ca35ed71d
Haojian Zhuang <haojian.zhuang@linaro.org>
2020-06-11 15:49:55 +0100

    conf.sh: enable perf as reference
    Enable perf in "conf.sh" for reference.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- conf.sh
+++ conf.sh
@@ -9,6 +9,7 @@
 #
 
 ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes ./configure \
+	--enable-perf=yes \
 	--host aarch64-linux-gnu \
 	--target aarch64-linux-gnu \
 	--program-prefix aarch64-linux-gnu-
