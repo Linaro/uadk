fb86846f31937a78c9819c10b4e47420709dea0b
mizhenkun <mi_zhenkun@163.com>
2020-07-21 16:08:15 +0800

    Realize the user sample scheduler's global sched information and some inner function of pick next.

--- user_sched_sample.c
+++ user_sched_sample.c
@@ -2,6 +2,8 @@
 #include <wd_comp.h>
 
 
+#define MAX_CTX_NUM 1024
+
 enum sched_x_pos {
 	X_SYNC = 0,
 	X_ASYNC = 1,
@@ -15,73 +17,98 @@ enum sched_y_pos {
 };
 
 /**
- * struct sched_ctx_info - define one ctx pos.
+ * struct sched_ctx_range - define one ctx pos.
  * @begin: the start pos in ctxs of config.
  * @end: the end pos in ctxx of config.
+ * @last: the last one which be distributed
  */
-struct sched_ctx_pos {
+struct sched_ctx_region {
 	int begin;
 	int end;
+	int last;
 };
 
 /**
- * sched_ctx_map - define the map for the comp ctxs, using for quickly search
- *                 the x range: two(sync and async), the y range: two(comp and uncomp)
- *                 the map[x][y]'s value is the ctx begin and end pos
+ * sample_sched_info - define the context of the scheduler
+ * @ctx_region: define the map for the comp ctxs, using for quickly search
+				the x range: two(sync and async), the y range: two(comp and uncomp)
+				the map[x][y]'s value is the ctx begin and end pos
+ * @last_pos: record the last one pos which be distributed in different type ctxs
+ * @count: record the req count of ccontex
  */
-int sched_ctx_map[X_BUTT][Y_BUTT] = {
-	{0, 0}, {1, 1}, {2, 2}, {3, 3}
+struct sample_sched_info {
+	struct sched_ctx_region ctx_region[X_BUTT][Y_BUTT];
+	int count[MAX_CTX_NUM];
 };
 
-/* the send nums of every ctx */
-int sched_send_num[4] = {0};
+
+int sample_get_next_pos(struct sched_ctx_region *region) {
+	int pos = region->last;
+
+	if (pos < region->last) {
+		pos++;
+	} else if (pos == region->last) {
+		pos = region->begin;
+	} else {
+		/* If the pos's value is out of range, we can output the error info and correct the error */
+		printf("ERROR: pos = %d, begin = %d, end = %d", pos, region->begin, region->end);
+		pos = region->begin;
+	}
+	
+	region->last = pos;
+
+	return pos;
+}
 
 /**
- * sample_sched_init - initialize the global sched info
+ * sample_get_ctx_range - Get ctx range from ctx_map by the wd comp arg
  */
-void sample_sched_init(void *sched_ctx) {
-	return;
+struct sched_ctx_region *sample_get_ctx_range(struct wd_comp_arg *arg, struct sched_ctx_region **ctx_map)
+{
+	return NULL;
 }
 
 /**
  * ssample_pick_next_ctx - Get one ctx from ctxs by the sched_ctx and arg
- * 
+ *
  * This function will be registered to the wd comp
  */
 handle_t sample_pick_next_ctx(struct wd_ctx_config *cfg, void* sched_ctx, struct wd_comp_arg *arg)
 {
-	int x = X_SYNC;
-	int y = Y_COMP;
-	int pos = sched_ctx_map[x][y].begin;
+	int pos;
+	struct sched_ctx_region *region = NULL;
+	struct sample_sched_info *sched_info = (struct sample_sched_info*)sched_ctx;
 
-	sched_send_num[pos]++;
+	region = sample_get_ctx_range(arg, sched_info->ctx_region);
+	if (region) {
+		return NULL;
+	}
+
+	pos = sample_get_next_pos(region);
+
 	return cfg->ctxs[pos].ctx;
 }
 
 /**
  * sample_poll_policy - The polling policy matches the pick next ctx
- * 
+ *
  * This function will be registered to the wd comp 
  */
 __u32 sample_poll_policy(struct wd_ctx_config *cfg, void* sched_ctx)
 {
-	int x, y, begin, end;
-	int poll_num;
-	int pos;
-
-	x = X_ASYNC;
-	
-	for (y = 0; y < Y_BUTT; y++) {
-		begin = sched_ctx_map[x][y].begin;
-		end = sched_ctx_map[x][y].end;
-		for (pos = begin; pos <= end; pos++) {
-			poll_num = wd_comp_poll_ctx(cfg->ctxs[pos], sched_send_num[pos])
-			if (poll_num != sched_send_num[pos]) {
-				/* printf the error info */
-			}
-			sched_send_num[pos] = 0;
-		}
-	}
-	
 	return 0;
 }
+
+/**
+ * sample_sched_init - initialize the global sched info
+ */
+void sample_sched_init(void *sched_ctx) {
+	struct sample_sched_info *sched_info = (struct sample_sched_info*)sched_ctx;
+
+	memset(sched_info, 0, sizeof(struct sample_sched_info));
+
+	/* initialize the global sched info base the ctx allocation */
+
+	return;
+}
+
