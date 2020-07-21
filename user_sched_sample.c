a05c08a7da3da685dfb6f142e04d0c39cce714ef
mizhenku <mi_zhenkun@163.com>
2020-07-21 20:08:00 +0800

    Reserve the interface for differet sched mode in the future.

--- user_sched_sample.c
+++ user_sched_sample.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <wd_comp.h>
 
-
 #define MAX_CTX_NUM 1024
 
 enum sched_x_pos {
@@ -14,6 +13,11 @@ enum sched_y_pos {
 	Y_COMP = 0,
 	Y_UNCOMP = 1,
 	Y_BUTT
+};
+
+enum sched_mode {
+	SCHED_RR,
+	SCHED_BUTT
 };
 
 /**
@@ -39,10 +43,10 @@ struct sched_ctx_region {
 struct sample_sched_info {
 	struct sched_ctx_region ctx_region[X_BUTT][Y_BUTT];
 	int count[MAX_CTX_NUM];
+	int sched_mode;
 };
 
-
-int sample_get_next_pos(struct sched_ctx_region *region) {
+int sample_get_next_pos_rr(struct sched_ctx_region *region, void *para) {
 	int pos = region->last;
 
 	if (pos < region->last) {
@@ -51,13 +55,37 @@ int sample_get_next_pos(struct sched_ctx_region *region) {
 		pos = region->begin;
 	} else {
 		/* If the pos's value is out of range, we can output the error info and correct the error */
-		printf("ERROR: pos = %d, begin = %d, end = %d", pos, region->begin, region->end);
+		printf("ERROR: pos = %d, begin = %d, end = %d\n", pos, region->begin, region->end);
 		pos = region->begin;
 	}
-	
+
 	region->last = pos;
 
 	return pos;
+}
+
+/**
+ * sample_get_next_pos - Find the sched function.
+ */
+int sample_get_next_pos(struct sched_ctx_region *region, void *para, int mode)
+{
+	int i;
+	static struct {
+		int mode;
+		int (*func)(struct sched_ctx_region *region, void *para);
+	}sched_func[] = {
+		{SCHED_RR, sample_get_next_pos_rr},
+	};
+
+	for (i = 0; i < SCHED_BUTT; i++) {
+		if (mode == sched_func[i].mode) {
+			return sched_func[i].func(region, para);
+		}
+	}
+
+	printf("ERROR: The sched_mode : %d is not support.\n", mode);
+
+	return 0;
 }
 
 /**
@@ -69,6 +97,15 @@ struct sched_ctx_region *sample_get_ctx_range(struct wd_comp_arg *arg, struct sc
 }
 
 /**
+ * Fill para that the different mode needs
+ */
+void sample_get_para(int sched_mode, void *para)
+{
+	/* Different mode attention different attribute, we need define the attribute for the mode */
+	return;
+}
+
+/**
  * ssample_pick_next_ctx - Get one ctx from ctxs by the sched_ctx and arg
  *
  * This function will be registered to the wd comp
@@ -76,6 +113,7 @@ struct sched_ctx_region *sample_get_ctx_range(struct wd_comp_arg *arg, struct sc
 handle_t sample_pick_next_ctx(struct wd_ctx_config *cfg, void* sched_ctx, struct wd_comp_arg *arg)
 {
 	int pos;
+	void *para;
 	struct sched_ctx_region *region = NULL;
 	struct sample_sched_info *sched_info = (struct sample_sched_info*)sched_ctx;
 
@@ -83,8 +121,10 @@ handle_t sample_pick_next_ctx(struct wd_ctx_config *cfg, void* sched_ctx, struct
 	if (region) {
 		return NULL;
 	}
-
-	pos = sample_get_next_pos(region);
+	
+	/* Different sched policy mybe need some specific para. */
+	sample_get_para(sched_info->sched_mode, arg, para);
+	pos = sample_get_next_pos(region, para, sched_info->sched_mode);
 
 	return cfg->ctxs[pos].ctx;
 }
@@ -92,7 +132,7 @@ handle_t sample_pick_next_ctx(struct wd_ctx_config *cfg, void* sched_ctx, struct
 /**
  * sample_poll_policy - The polling policy matches the pick next ctx
  *
- * This function will be registered to the wd comp 
+ * This function will be registered to the wd comp
  */
 __u32 sample_poll_policy(struct wd_ctx_config *cfg, void* sched_ctx)
 {
@@ -108,7 +148,7 @@ void sample_sched_init(void *sched_ctx) {
 	memset(sched_info, 0, sizeof(struct sample_sched_info));
 
 	/* initialize the global sched info base the ctx allocation */
+	sched_info.sched_mode = SCHED_RR;
 
 	return;
 }
-
