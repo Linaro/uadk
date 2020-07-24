1083b46a4142a6d35a5c4caeb6f20d85c4684309
mizhenkun <mi_zhenkun@163.com>
2020-07-24 11:45:26 +0800

    Add get sched region for user scheduler sample.

--- user_sched_sample.c
+++ user_sched_sample.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/poll.h>
@@ -124,10 +125,12 @@ struct sched_operator sched_ops[SCHED_BUTT] = {
 /**
  * sample_get_ctx_range - Get ctx range from ctx_map by the wd comp arg
  */
-struct sched_ctx_region* sample_get_ctx_range(struct wd_comp_arg *req,
-	struct sched_ctx_region (*ctx_map)[Y_BUTT])
+struct sched_ctx_region* sample_get_ctx_range(struct wd_comp_arg *req, struct sched_ctx_region (*ctx_map)[Y_BUTT])
 {
-	return NULL;
+	int x = req->flag;
+	int y = req->status;
+
+	return &ctx_map[x][y];
 }
 
 /**
@@ -135,14 +138,14 @@ struct sched_ctx_region* sample_get_ctx_range(struct wd_comp_arg *req,
  *
  * This function will be registered to the wd comp
  */
-handle_t sample_pick_next_ctx(struct wd_ctx_config *cfg, void *sched_ctx, struct wd_comp_arg *req)
+handle_t sample_pick_next_ctx(struct wd_ctx_config *cfg, void *sched_ctx, struct wd_comp_arg *req, int numa_id)
 {
 	int pos;
 	void *para = NULL;
 	struct sched_ctx_region *region = NULL;
 	struct sample_sched_info *sched_info = (struct sample_sched_info*)sched_ctx;
 
-	region = sample_get_ctx_range(req, sched_info->ctx_region);
+	region = sample_get_ctx_range(req, sched_info[numa_id].ctx_region);
 	if (!region) {
 		return (handle_t)NULL;
 	}
@@ -164,10 +167,12 @@ handle_t sample_pick_next_ctx(struct wd_ctx_config *cfg, void *sched_ctx, struct
 __u32 sample_poll_policy(struct wd_ctx_config *cfg, void *sched_ctx)
 {
 	int numa_id;
-	struct sample_sched_info *sched_info = NULL;
+	struct sample_sched_info *sched_info = (struct sample_sched_info *)sched_ctx;
 
 	for (numa_id = 0; numa_id < MAX_NUMA_NUM; numa_id++) {
-		sched_ops[g_sched_mode].poll_policy(cfg, sched_info[numa_id].ctx_region);
+		if (sched_info[numa_id].valid) {
+			sched_ops[g_sched_mode].poll_policy(cfg, sched_info[numa_id].ctx_region);
+		}
 	}
 
 	return 0;
