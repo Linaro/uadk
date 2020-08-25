4ece3b97a406658a109310b22808fce839125a7a
Zhou Wang <wangzhou1@hisilicon.com>
2020-08-25 23:28:49 +0800

    wd_rsa/hisi_hpre: remove driver related head files as sec did
    Just remove codes. Let's do related clean up in next patch.
    
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- drv/hisi_hpre.c
+++ drv/hisi_hpre.c
@@ -12,7 +12,6 @@
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
 #include <sys/types.h>
-#include "hisi_hpre.h"
 #include "../include/drv/wd_rsa_drv.h"
 
 #define HPRE_HW_TASK_DONE	3
@@ -22,6 +21,72 @@
 #define QM_HADDR_SHIFT		32
 #define LW_U32(pa)	((__u32)((pa) & QM_L32BITS_MASK))
 #define HI_U32(pa)	((__u32)(((pa) >> QM_HADDR_SHIFT) & QM_L32BITS_MASK))
+
+#include "hisi_qm_udrv.h"
+#include "smm.h"
+#include "wd.h"
+#include "wd_sched.h"
+
+#define BYTE_BITS			8
+#define BYTE_BITS_SHIFT		3
+#define CRT_PARAMS_SZ(key_size)		((5 * (key_size)) >> 1)
+#define CRT_GEN_PARAMS_SZ(key_size)	((7 * (key_size)) >> 1)
+#define GEN_PARAMS_SZ(key_size)		((key_size) << 1)
+#define CRT_PARAM_SZ(key_size)		((key_size) >> 1)
+#define GET_NEGATIVE(val)	(0 - (val))
+#define XTS_MODE_KEY_DIVISOR	2
+#define AES_KEYSIZE_128		16
+#define AES_KEYSIZE_192		24
+#define AES_KEYSIZE_256		32
+#define CTX_ID_MAX_NUM		64
+
+enum hpre_alg_type {
+	HPRE_ALG_NC_NCRT = 0x0,
+	HPRE_ALG_NC_CRT = 0x1,
+	HPRE_ALG_KG_STD = 0x2,
+	HPRE_ALG_KG_CRT = 0x3,
+	HPRE_ALG_DH_G2 = 0x4,
+	HPRE_ALG_DH = 0x5,
+	HPRE_ALG_PRIME = 0x6,
+	HPRE_ALG_MOD = 0x7,
+	HPRE_ALG_MOD_INV = 0x8,
+	HPRE_ALG_MUL = 0x9,
+	HPRE_ALG_COPRIME = 0xA,
+	HPRE_ALG_ECC_CURVE_TEST = 0xB,
+	HPRE_ALG_ECDH_PLUS = 0xC,
+	HPRE_ALG_ECDH_MULTIPLY = 0xD,
+	HPRE_ALG_ECDSA_SIGN = 0xE,
+	HPRE_ALG_ECDSA_VERF = 0xF,
+	HPRE_ALG_X_DH_MULTIPLY = 0x10,
+	HPRE_ALG_SM2_KEY_GEN = 0x11,
+	HPRE_ALG_SM2_SIGN = 0x12,
+	HPRE_ALG_SM2_VERF = 0x13,
+	HPRE_ALG_SM2_ENC = 0x14,
+	HPRE_ALG_SM2_DEC = 0x15
+};
+
+/* I think put venodr hw msg as a user interface is not suitable here */
+struct hisi_hpre_sqe {
+	__u32 alg	: 5;
+
+	/* error type */
+	__u32 etype	:11;
+	__u32 resv0	: 14;
+	__u32 done	: 2;
+	__u32 task_len1	: 8;
+	__u32 task_len2	: 8;
+	__u32 mrttest_num : 8;
+	__u32 resv1	: 8;
+	__u32 low_key;
+	__u32 hi_key;
+	__u32 low_in;
+	__u32 hi_in;
+	__u32 low_out;
+	__u32 hi_out;
+	__u32 tag	:16;
+	__u32 resv2	:16;
+	__u32 rsvd1[7];
+};
 
 struct hisi_hpre_ctx {
 	struct wd_ctx_config	config;
