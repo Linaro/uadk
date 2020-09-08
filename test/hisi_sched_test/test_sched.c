7f6960345b7e2ac44cc81f4260c0c16a3142096e
mizhenkun <mizhenkun@huawei.com>
2020-09-08 20:56:35 +0800

    Update the test code of the schedule.

--- test/hisi_sched_test/test_sched.c
+++ test/hisi_sched_test/test_sched.c
@@ -8,7 +8,7 @@
 
 struct wd_ctx_config g_sched_test_cfg;
 
-int sched_test_init()
+int sched_config_init()
 {
 	__u32 i;
 
@@ -28,18 +28,18 @@ int sched_test_init()
 	return SCHED_SUCCESS;
 }
 
-static void sched_test_exit()
+static void sched_config_release()
 {
 	free(g_sched_test_cfg.ctxs);
 }
 
-static int user_poll_func_stub_succee(handle_t h_ctx, __u32 expect, __u32 *count)
+static int user_poll_func_stub_succee(__u32 pos, __u32 expect, __u32 *count)
 {
 	*count = expect;
 	return SCHED_SUCCESS;
 }
 
-static int user_poll_func_stub_fail(handle_t h_ctx, __u32 expect, __u32 *count)
+static int user_poll_func_stub_fail(__u32 pos, __u32 expect, __u32 *count)
 {
 	return SCHED_SUCCESS;
 }
@@ -47,19 +47,20 @@ static int user_poll_func_stub_fail(handle_t h_ctx, __u32 expect, __u32 *count)
 static int sched_test_case1(const char *name)
 {
 	int ret;
+	struct wd_sched *sched;
 
-	ret = sample_sched_init(SCHED_POLICY_BUTT, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
-	if (ret != SCHED_PARA_INVALID) {
+	sched = sample_sched_alloc(SCHED_POLICY_BUTT, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
+	if (sched) {
 		printf("CASE 1: %s failure, sched_type check failed\n", name);
 		return SCHED_ERROR;
 	}
 
-	ret = sample_sched_init(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, NULL);
-	if (ret != SCHED_PARA_INVALID) {
+	sched = sample_sched_alloc(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, NULL);
+	if (sched) {
 		printf("CASE 1: %s failure, func check failed\n", name);
 		return SCHED_ERROR;
 	}
-	
+
 	printf("CASE 1: %s success.\n", name);
 	return SCHED_SUCCESS;
 }
@@ -67,32 +68,36 @@ static int sched_test_case1(const char *name)
 static int sched_test_case2(const char *name)
 {
 	int ret;
+	struct wd_sched *sched;
 
-	ret = sample_sched_init(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
-	if (ret) {
-		printf("CASE 2: %s failure, error case SCHED_POLICY_BUTT\n", name);
+	sched = sample_sched_alloc(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
+	if (!sched) {
+		printf("CASE 2: %s failure, sample_sched_alloc failed\n", name);
 		return SCHED_ERROR;
 	}
 
-	ret = sample_sched_fill_region(MAX_NUMA_NUM, 0, 0, 0, 0);
+	ret = sample_sched_fill_data(sched, MAX_NUMA_NUM, 0, 0, 0, 0);
 	if (ret != SCHED_PARA_INVALID) {
 		printf("CASE 2: %s failure, numa check failed\n", name);
 		return SCHED_ERROR;
 	}
 
-	ret = sample_sched_fill_region(0, 2, 0, 0, 0);
+	ret = sample_sched_fill_data(sched, 0, 2, 0, 0, 0);
 	if (ret != SCHED_PARA_INVALID) {
 		printf("CASE 2: %s failure, mode check failed\n", name);
 		return SCHED_ERROR;
 	}
 
-	ret = sample_sched_fill_region(0, 0, SCHED_TEST_TYPE_NUM, 0, 0);
+	ret = sample_sched_fill_data(sched, 0, 0, SCHED_TEST_TYPE_NUM, 0, 0);
 	if (ret != SCHED_PARA_INVALID) {
 		printf("CASE 2: %s failure, type check failed\n", name);
 		return SCHED_ERROR;
 	}
 
+	sample_sched_release(sched);
+
 	printf("CASE 2: %s success.\n", name);
+
 	return SCHED_SUCCESS;
 }
 
@@ -101,17 +106,18 @@ static int sched_test_case3(const char *name)
 	int ret;
 	int i;
 	char req[] = "Request stub";
-	handle_t ctx;
+	__u32 pos;
 	struct sched_key key;
+	struct wd_sched *sched;
 
-	ret = sample_sched_init(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
-	if (ret) {
-		printf("CASE 3: %s failure, sample_sched_init\n", name);
+	sched = sample_sched_alloc(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
+	if (!sched) {
+		printf("CASE 3: %s failure, sample_sched_alloc failed\n", name);
 		return SCHED_ERROR;
 	}
 
 	/* case: numa:0, mode:0, type:0, ctxs:0-99 */
-	ret = sample_sched_fill_region(0, 0, 0, 0, 99);
+	ret = sample_sched_fill_data(sched, 0, 0, 0, 0, 99);
 	if (ret) {
 		printf("CASE 3: %s failure, sample_sched_fill_region\n", name);
 		return SCHED_ERROR;
@@ -122,27 +128,25 @@ static int sched_test_case3(const char *name)
 	key.mode = 0;
 	key.type = 0;
 	for (i = 0; i < SCHED_TEST_CTX_NUM; i++) {
-		ctx = sample_sched_pick_next_ctx((const struct wd_ctx_config*)&g_sched_test_cfg, req, &key);
-		if (!ctx) {
-			printf("CASE 3: %s failure, sample_sched_pick_next_ctx\n", name);
-			return SCHED_ERROR;
-		}
-
+		pos = sched->pick_next_ctx(sched->h_sched_ctx, req, &key);
 		/* 0-99 pos to 1-100 ctx, reference the sched_test_init */
-		if (ctx != i + 1) {
+		if (pos != i) {
 			printf("CASE 3: %s failure, ctx check failed\n", name);
 			return SCHED_ERROR;
 		}
 	}
 
-	/* Test times bigger than SCHED_TEST_CTX_NUM, the ctx must start from 1 */
-	ctx = sample_sched_pick_next_ctx((const struct wd_ctx_config*)&g_sched_test_cfg, req, &key);
-	if (ctx != 1) {
+	/* Test times bigger than SCHED_TEST_CTX_NUM, the ctx must start from 0 */
+	pos = sched->pick_next_ctx(sched->h_sched_ctx, req, &key);
+	if (pos != 0) {
 		printf("CASE 3: %s failure, ctx cycle check failed\n", name);
 		return SCHED_ERROR;
 	}
 
+	sample_sched_release(sched);
+
 	printf("CASE 3: %s success.\n", name);
+
 	return SCHED_SUCCESS;
 }
 
@@ -150,28 +154,32 @@ int sched_test_case4(const char *name)
 {
 	int ret;
 	__u32 count = 0;
+	struct wd_sched *sched;
 
-	ret = sample_sched_init(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
-	if (ret) {
-		printf("CASE 4: %s failure, sample_sched_init\n", name);
+	sched = sample_sched_alloc(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_succee);
+	if (!sched) {
+		printf("CASE 4: %s failure, sample_sched_alloc failed\n", name);
 		return SCHED_ERROR;
 	}
 
 	/* case: numa:1, mode:1, type:1, ctxs:25-75 */
-	ret = sample_sched_fill_region(1, 1, 1, 25, 75);
+	ret = sample_sched_fill_data(sched, 1, 1, 1, 25, 75);
 	if (ret) {
 		printf("CASE 4: %s failure, sample_sched_fill_region\n", name);
 		return SCHED_ERROR;
 	}
 
 	/* 100 is expect poll times */
-	ret = sample_sched_poll_policy((const struct wd_ctx_config*)&g_sched_test_cfg, 100, &count);
+	ret = sched->poll_policy(sched->h_sched_ctx, &g_sched_test_cfg, 100, &count);
 	if (ret || count != 100) {
 		printf("CASE 4: %s failure, sample_sched_poll_policy, count = %u\n", name, count);
 		return SCHED_ERROR;
 	}
 
+	sample_sched_release(sched);
+
 	printf("CASE 4: %s success.\n", name);
+
 	return SCHED_SUCCESS;
 }
 
@@ -179,28 +187,32 @@ int sched_test_case5(const char *name)
 {
 	int ret;
 	__u32 count = 0;
+	struct wd_sched *sched;
 
-	ret = sample_sched_init(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_fail);
-	if (ret) {
-		printf("CASE 5: %s failure, sample_sched_init\n", name);
+	sched = sample_sched_alloc(SCHED_POLICY_RR, SCHED_TEST_TYPE_NUM, user_poll_func_stub_fail);
+	if (!sched) {
+		printf("CASE 5: %s failure, sample_sched_alloc failed\n", name);
 		return SCHED_ERROR;
 	}
 
-	/* case: numa:2, mode:1, type:3, ctxs: 0-5 */
-	ret = sample_sched_fill_region(2, 1, 3, 0, 5);
+	/* case: numa:1, mode:1, type:3, ctxs: 0-5 */
+	ret = sample_sched_fill_data(sched, 1, 1, 3, 0, 5);
 	if (ret) {
 		printf("CASE 5: %s failure, sample_sched_fill_region\n", name);
 		return SCHED_ERROR;
 	}
 
 	/* 100 is expect poll times */
-	ret = sample_sched_poll_policy((const struct wd_ctx_config*)&g_sched_test_cfg, 1, &count);
+	ret = sched->poll_policy(sched->h_sched_ctx, &g_sched_test_cfg, 1, &count);
 	if (ret || count != 0) {
 		printf("CASE 5: %s failure, sample_sched_poll_policy, count = %u\n", name, count);
 		return SCHED_ERROR;
 	}
 
+	sample_sched_release(sched);
+
 	printf("CASE 5: %s success.\n", name);
+
 	return SCHED_SUCCESS;
 }
 
@@ -222,16 +234,14 @@ void main()
 			{"Poll RR safety test", sched_test_case5},
 	};
 
-	sched_test_init();
-
+	sched_config_init();
 	for (i = 0; i < sizeof(test_case) / sizeof(struct test_case); i++) {
 		total++;
 		ret = test_case[i].func(test_case[i].name);
 		if (ret)
 			fail++;
 	}
-
-	sched_test_exit();
+	sched_config_release();
 
 	printf("Total   Num: %d.\n", total);
 	printf("SUCCESS Num: %d.\n", total - fail);
