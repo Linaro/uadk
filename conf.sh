cd3a05ebd19e18dc3712cea707be6e181cc8d3e4
Haojian Zhuang <haojian.zhuang@linaro.org>
2020-11-11 08:06:22 +0000

    Revert "scripts: remove unused scripts"
    This reverts commit 5dc337f09572c4aaaaacf7d3d506ab548ef4e73a.

---
+++ conf.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#to build hpre
+# git clone https://github.com/openssl/openssl.git
+# cd openssl
+# ./Configure linux-aarch64 --cross-compile-prefix=aarch64-linux-gnu-
+# add the following configure to this project (assume it is in paralle dir):
+# --with-openssl_dir=`pwd`/../openssl
+#
+
+COMPILE_TYPE="--disable-static --enable-shared"
+
+if [ $1 ]; then
+	if [ $1 = "--static" ]; then
+		echo "configure to static compile!"
+		COMPILE_TYPE="--enable-static --disable-shared --with-static_drv"
+	else
+		echo "invalid paramter, --static is static compile, compile to shared lib by default"
+	fi
+fi
+
+ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes ./configure \
+	--enable-perf=yes \
+	--host aarch64-linux-gnu \
+	--target aarch64-linux-gnu \
+	--includedir=/usr/local/include/uadk \
+	$COMPILE_TYPE
