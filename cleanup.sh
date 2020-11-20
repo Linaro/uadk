e074b5329c16cc0f0d04963b02ab78edf42e6a17
Haojian Zhuang <haojian.zhuang@linaro.org>
2020-11-20 09:18:39 +0000

    wd: build mem testcases for uadk v1
    Build memory testcases of UADK v1.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- cleanup.sh
+++ cleanup.sh
@@ -8,7 +8,12 @@ FILES="aclocal.m4 autom4te.cache compile config.guess config.h.in config.log \
        config.status config.sub configure cscope.out depcomp install-sh      \
        libsrc/Makefile libsrc/Makefile.in libtool ltmain.sh Makefile         \
        ar-lib m4 Makefile.in missing src/Makefile src/Makefile.in	     \
-       test/Makefile.in test/hisi_hpre_test/Makefile.in			     \
-       v1/Makefile.in v1/Makefile.in"
+       test/Makefile test/Makefile.in test/hisi_hpre_test/Makefile.in	     \
+       test/hisi_hpre_test/Makefile test/hisi_sec_test/Makefile              \
+       test/hisi_sec_test/Makefile.in test/hisi_zip_test/Makefile            \
+       test/hisi_zip_test/Makefile.in					     \
+       v1/Makefile.in v1/Makefile.in v1/test/Makefile v1/test/Makefile.in    \
+       v1/test/test_mm/Makefile v1/test/test_mm/Makefile.in		     \
+       v1/test/bmm_test/Makefile v1/test/bmm_test/Makefile.in"
 
 rm -vRf $FILES *~
