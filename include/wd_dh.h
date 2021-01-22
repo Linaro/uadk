f5768c18d6ddaaef05cfa49a76172da92abd5db5
Hui Tang <tanghui20@huawei.com>
2021-01-22 16:44:28 +0800

    wd/hpre: Fix clean code
    Signed-off-by: Hui Tang <tanghui20@huawei.com>

--- include/wd_dh.h
+++ include/wd_dh.h
@@ -54,7 +54,7 @@ handle_t wd_dh_alloc_sess(struct wd_dh_sess_setup *setup);
 void wd_dh_free_sess(handle_t sess);
 int wd_do_dh_async(handle_t sess, struct wd_dh_req *req);
 int wd_do_dh_sync(handle_t sess, struct wd_dh_req *req);
-int wd_dh_poll_ctx(__u32 index, __u32 expt, __u32 *count);
+int wd_dh_poll_ctx(__u32 idx, __u32 expt, __u32 *count);
 int wd_dh_poll(__u32 expt, __u32 *count);
 int wd_dh_init(struct wd_ctx_config *config, struct wd_sched *sched);
 void wd_dh_uninit(void);
--- include/wd_ecc.h
+++ include/wd_ecc.h
@@ -468,7 +468,7 @@ extern int wd_do_ecc_async(handle_t sess, struct wd_ecc_req *req);
 
 /**
  * wd_ecc_poll_ctx() - Poll a ctx.
- * @pos:	The ctx index which will be polled.
+ * @pos:	The ctx idx which will be polled.
  * @expt:	Max number of requests to poll. If 0, polled all finished
  *		requests in this ctx.
  * @count:	The number of polled requests.
@@ -478,7 +478,7 @@ extern int wd_do_ecc_async(handle_t sess, struct wd_ecc_req *req);
  * User defines polling policy in poll_policiy, when it needs to poll a
  * specific ctx, this function should be used.
  */
-extern int wd_ecc_poll_ctx(__u32 index, __u32 expt, __u32 *count);
+extern int wd_ecc_poll_ctx(__u32 idx, __u32 expt, __u32 *count);
 
 #ifdef __cplusplus
 }
