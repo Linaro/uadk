3bf5f83a713fd212849a2b4b25cba7cd21235e7f
Hui Tang <tanghui20@huawei.com>
2021-01-22 16:49:51 +0800

    wd/hpre: fix sm2/x25519/x448
    1.Init rand method.
    2.Init x25519 and x448 sqe alg.
    
    Signed-off-by: Hui Tang <tanghui20@huawei.com>

--- drv/hisi_hpre.c
+++ drv/hisi_hpre.c
@@ -734,7 +734,10 @@ static int ecc_prepare_alg(struct wd_ecc_msg *msg,
 	case HPRE_SM2_ENC: /* fall through */
 	case HPRE_SM2_DEC: /* fall through */
 	case WD_ECXDH_COMPUTE_KEY:
-		hw_msg->alg = HPRE_ALG_ECDH_MULTIPLY;
+		if (msg->curve_id == WD_X448 || msg->curve_id == WD_X25519)
+			hw_msg->alg = HPRE_ALG_X_DH_MULTIPLY;
+		else
+			hw_msg->alg = HPRE_ALG_ECDH_MULTIPLY;
 		break;
 	default:
 		return -WD_EINVAL;
@@ -840,8 +843,16 @@ static int ecc_prepare_prikey(struct wd_ecc_key *key, void **data, int id)
 		return ret;
 
 	/*
-	 * This is a pretreatment of x25519/x448, as described in RFC7748
-	 * hpre is big-endian, so the byte is opposite.
+	 * This is a pretreatment of X25519/X448, as described in RFC7748:
+	 * For X25519, in order to decode 32 random bytes as an integer
+	 * scaler, set the three LSB of the first byte and MSB of the last
+	 * to zero, set the second MSB of the last byte to 1.
+	 * For X448, set the two LSB of the first byte to 0, and MSB of the
+	 * last byte to 1. Decode in little-endian mode.
+	 * HPRE hardware module uses big-endian mode, so the bytes to be
+	 * set are reversed compared to RFC7748:
+	 * For example, dat[0] of X25519 in RFC7748 is reversed to dat[31]
+	 * in HPRE specification, so does X448.
 	 */
 	dat = d->data;
 	bsize = d->bsize;
