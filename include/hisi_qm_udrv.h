fe1e2c9e33347f9ec803f5c88a5f3cb596cd3339
Haojian Zhuang <haojian.zhuang@linaro.org>
2021-04-01 09:52:08 +0800

    debug: try to use lockfree on QM
    Concerns:
    1. Two threads (T1 & T2) are sending messages in system.
       T1                        T2
       updated tail
                                 updated tail
                                 trigger doorbell register
       (T2's tail must be larger than T1's tail.)
       (Now doorbell is triggered by T2. T1 shouldn't access T1 any more.)
       (If T1 doesn't know it, continue to touch doorbell. What happens?)
    
    2. Not finished yet.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- include/hisi_qm_udrv.h
+++ include/hisi_qm_udrv.h
@@ -39,6 +39,21 @@ struct hisi_qm_priv {
 	__u16 op_type;
 };
 
+struct hisi_qm_sq {
+	__u16	head;
+	__u16	tail;
+};
+
+/* how to check whether receiving queue is empty or not */
+struct hisi_qm_cq {
+	__u16	head;
+};
+
+struct hisi_concurrent_queue {
+	__u16	idx;
+	__u16	tag;
+};
+
 struct hisi_qm_queue_info {
 	void *sq_base;
 	void *cq_base;
@@ -49,6 +64,13 @@ struct hisi_qm_queue_info {
 		  __u16 index, __u8 priority);
 	void *ds_tx_base;
 	void *ds_rx_base;
+	struct hisi_concurrent_queue	ccrnt_head;
+	struct hisi_concurrent_queue	ccrnt_tail;
+	/*
+	 *
+	 */
+	struct hisi_concurrent_queue	ccrnt_hw;
+	struct hisi_concurrent_queue	ccrnt_phase;
 	__u16 sq_tail_index;
 	__u16 sq_head_index;
 	__u16 cq_head_index;
