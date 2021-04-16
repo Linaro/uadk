6dc04f5a0191be7c1ad58f95099ef3ec97497d09
Yang Shen <shenyang39@huawei.com>
2021-04-16 15:52:21 +0800

    uadk: test/zip - support test lz77_zstd\sgl
    1:use '-Z' to test lz77_zstd algorithm
    2:use '-L' to test sgl type buffer
    
    Signed-off-by: Yang Shen <shenyang39@huawei.com>

--- test/hisi_zip_test/sva_file_test.c
+++ test/hisi_zip_test/sva_file_test.c
@@ -23,6 +23,7 @@
 #include <sched.h>
 #include <sys/time.h>
 #include <sys/syscall.h>
+#include <math.h>
 
 #include "wd.h"
 #include "hisi_qm_udrv.h"
@@ -30,6 +31,7 @@
 
 
 struct test_zip_pthread_dt {
+	__u8 data_fmt;
 	int cpu_id;
 	int thread_num;
 	int alg_type;
@@ -43,6 +45,7 @@ struct test_zip_pthread_dt {
 	__u32 dst_len;
 	float com_time;
 	float decom_time;
+	void *priv;
 };
 
 struct user_comp_param {
@@ -101,6 +104,7 @@ therad_no_affinity:
 		if (pdata->op_type == WD_DIR_COMPRESS) {
 			ret = hw_stream_compress(pdata->alg_type,
 					pdata->blksize,
+					pdata->data_fmt,
 					pdata->dst,
 					&pdata->dst_len,
 					pdata->src,
@@ -111,6 +115,7 @@ therad_no_affinity:
 		} else if (pdata->op_type == WD_DIR_DECOMPRESS) {
 			ret = hw_stream_decompress(pdata->alg_type,
 					pdata->blksize,
+					pdata->data_fmt,
 					pdata->dst,
 					&pdata->dst_len,
 					pdata->src,
@@ -123,6 +128,7 @@ therad_no_affinity:
 		if (verify) {
 			ret = hw_stream_decompress(pdata->alg_type,
 						   pdata->blksize,
+						   pdata->data_fmt,
 						   pdata->src,
 						   &pdata->src_len,
 						   pdata->dst,
@@ -176,19 +182,23 @@ void *zip_sys_async_test_poll_thread(void *args)
 	cpu_id = pdata->cpu_id;
 	tid = gettid();
 
+	WD_ERR("%s start\n", __func__);
 
 	do {
 		count = 0;
-		dbg("poll start, expt =%d , have =%d!\n", pdata->iteration, totalcount);
+		dbg("poll start, expt =%d , have =%d!\n",
+		    pdata->iteration, totalcount);
 		ret = wd_comp_poll(pdata->iteration, &count);
 		if (ret < 0)
-			WD_ERR("poll fail! thread_id=%d, tid=%d. ret:%d\n", cpu_id, (int)tid, ret);
+			WD_ERR("poll fail! thread_id=%d, tid=%d. ret:%d\n",
+			       cpu_id, (int)tid, ret);
 		if (count > 0)
 			recnt = 0;
 		totalcount += count;
 		if (totalcount < pdata->iteration * pdata->thread_num) {
 			usleep(100000);
-			dbg("poll thread now no task, expt =%d , have =%d!\n", pdata->iteration, totalcount);
+			dbg("poll thread now no task, expt =%d , have =%d!\n",
+			    pdata->iteration, totalcount);
 			if (++recnt > MAX_POLL_COUNTS) {
 				WD_ERR("poll thread  no task, 1s timeout, expt =%d , have =%d!\n", pdata->iteration * pdata->thread_num, totalcount);
 				break;
@@ -197,11 +207,41 @@ void *zip_sys_async_test_poll_thread(void *args)
 
 	} while (totalcount < pdata->iteration * pdata->thread_num);
 
-	WD_ERR("thread_id = %d, test poll end, count=%d\n", pdata->cpu_id, totalcount);
+	WD_ERR("thread_id = %d, test poll end, count=%d\n",
+	       pdata->cpu_id, totalcount);
 
 	pdata->dst_len = out_len;
 
 	return NULL;
+}
+
+static struct wd_datalist *get_datalist(void *addr, __u32 size)
+{
+	int count = (int)ceil((double)size / SGE_SIZE);
+	struct wd_datalist *head, *cur, *tmp;
+	int i;
+
+	head = calloc(1, sizeof(struct wd_datalist));
+	if (!head) {
+		WD_ERR("failed to alloc datalist head\n");
+		return NULL;
+	}
+
+	cur = head;
+
+	for (i = 0; i < count; i++) {
+		cur->data = addr;
+		cur->len = (size > SGE_SIZE) ? SGE_SIZE : size;
+		addr += SGE_SIZE;
+		size -= SGE_SIZE;
+		if (i != count - 1) {
+			tmp = calloc(1, sizeof(struct wd_datalist));
+			cur->next = tmp;
+			cur = tmp;
+		}
+	}
+
+	return head;
 }
 
 void *zip_sys_async_test_thread(void *args)
@@ -215,6 +255,7 @@ void *zip_sys_async_test_thread(void *args)
 	int loop;
 	handle_t h_sess;
 	struct wd_comp_sess_setup setup;
+	struct wd_datalist *list;
 	struct wd_comp_req req;
 	__u32 count = 0;
 	int ret = 0;
@@ -251,13 +292,24 @@ therad_no_affinity:
 	u_param.cpu_id = cpu_id;
 	u_param.tid = tid;
 
-	req.src = pdata->src;
+	if (pdata->data_fmt) {
+		WD_ERR("now sge size is %u\n", SGE_SIZE);
+		list = get_datalist(pdata->src, pdata->src_len);
+		req.list_src = list;
+		list = get_datalist(pdata->dst, pdata->dst_len);
+		req.list_dst = list;
+	} else {
+		req.src = pdata->src;
+		req.dst = pdata->dst;
+	}
+
 	req.src_len = pdata->src_len;
-	req.dst = pdata->dst;
 	req.dst_len = pdata->dst_len;
 	req.op_type = pdata->op_type;
 	req.cb = (void *)zip_callback;
 	req.cb_param = &u_param;
+	req.data_fmt = pdata->data_fmt;
+	req.priv = pdata->priv;
 
 	dbg("%s:input req: src:%p, dst:%p,src_len: %d, dst_len:%d\n",
 	    __func__, req.src, req.dst, req.src_len, req.dst_len);
@@ -270,7 +322,8 @@ therad_no_affinity:
 		do {
 			ret = wd_do_comp_async(h_sess, &req);
 			if (ret == -EBUSY) {
-				WD_ERR("%s(): async test no cache,wait 10ms!\n", __func__);
+				WD_ERR("%s(): async test no cache,wait 10ms!\n",
+				       __func__);
 				usleep(10000);
 				continue;
 			}
@@ -326,26 +379,29 @@ therad_no_affinity:
 	do {
 		if (pdata->op_type == WD_DIR_COMPRESS) {
 			ret = hw_blk_compress(pdata->alg_type, pdata->blksize,
+					      pdata->data_fmt, pdata->priv,
 					      pdata->dst, &pdata->dst_len,
 					      pdata->src, pdata->src_len);
 			if (ret < 0)
 				WD_ERR("comp fail! thread_id=%d, tid=%d\n",
-					cpu_id, (int)tid);
+				       cpu_id, (int)tid);
 		} else if (pdata->op_type == WD_DIR_DECOMPRESS) {
 			ret = hw_blk_decompress(pdata->alg_type, pdata->blksize,
+					        pdata->data_fmt,
 						pdata->dst, &pdata->dst_len,
 						pdata->src, pdata->src_len);
 			if (ret < 0)
 				WD_ERR("decomp fail! thread_id=%d, tid=%d\n",
-					cpu_id, (int)tid);
+				       cpu_id, (int)tid);
 		}
 		if (verify) {
 			ret = hw_blk_decompress(pdata->alg_type, pdata->blksize,
+					        pdata->data_fmt,
 						pdata->src, &pdata->src_len,
 						pdata->dst, pdata->dst_len);
 			if (ret < 0)
 				WD_ERR("loop verify fail! ret=%d, id=%d\n",
-					ret, cpu_id);
+				       ret, cpu_id);
 			else
 				dbg("loop verify success! id=%d\n", cpu_id);
 		}
@@ -357,42 +413,110 @@ therad_no_affinity:
 
 #define MAX_LEN (~0U)
 
+struct seqdef {
+	__u32 offset;
+	__u16 litlen;
+	__u16 matchlen;
+};
+
+static int write_zstd_file(struct wd_lz77_zstd_data *output_format,
+			   __u8 data_fmt)
+{
+	struct wd_lz77_zstd_data *format = output_format;
+	struct wd_datalist *sgl;
+	int write_size = 0;
+	FILE *fout;
+	int ret;
+
+	dbg("%s start!\n", __func__);
+
+	fout = fopen("./zstd_lz77", "wb+");
+	if (fout == NULL) {
+		WD_ERR("file open failed\n");
+		return -1;
+	}
+
+	dbg("%s overflow cnt is %d\n", __func__, format->lit_length_overflow_cnt);
+	ret = fwrite(&format->lit_length_overflow_cnt, sizeof(__u32), 1, fout);
+	write_size += ret * sizeof(__u32);
+
+	dbg("%s overflow pos is %d\n", __func__, format->lit_length_overflow_pos);
+	ret = fwrite(&format->lit_length_overflow_pos, sizeof(__u32), 1, fout);
+	write_size += ret * sizeof(__u32);
+
+	dbg("%s literals number is %d\n", __func__, format->lit_num);
+	ret = fwrite(&format->lit_num, sizeof(__u32), 1, fout);
+	write_size += ret * sizeof(__u32);
+
+	if (data_fmt == WD_FLAT_BUF) {
+		dbg("%s literals address is %p\n", __func__, format->literals_start);
+		ret = fwrite(format->literals_start, sizeof(__u8), format->lit_num, fout);
+		write_size += ret * sizeof(__u8);
+	} else {
+		sgl = format->literals_start;
+		dbg("%s literals ori address is %p\n", __func__, sgl->data);
+		ret = fwrite(sgl->data, sizeof(__u8), format->lit_num, fout);
+		write_size += ret * sizeof(__u8);
+	}
+
+	dbg("%s sequences number is %d\n", __func__, format->seq_num);
+	ret = fwrite(&format->seq_num, sizeof(__u32), 1, fout);
+	write_size += ret * sizeof(__u32);
+
+	if (data_fmt == WD_FLAT_BUF) {
+		dbg("%s sequences address is %p\n", __func__, format->sequences_start);
+		ret = fwrite(format->sequences_start, sizeof(__u64), format->seq_num, fout);
+		write_size += ret * sizeof(__u64);
+	} else {
+		sgl = format->sequences_start;
+		dbg("%s sequences ori address is %p\n", __func__, sgl->data);
+		ret = fwrite(sgl->data, sizeof(__u64), format->seq_num, fout);
+		write_size += ret * sizeof(__u8);
+	}
+
+	dbg("%s write size is %d\n", __func__, write_size);
+
+	fclose(fout);
+
+	dbg("%s succeed!\n", __func__);
+
+	return write_size;
+}
+
 int comp_file_test(FILE *source, FILE *dest, struct test_options *opts)
 {
-	int fd;
-	struct stat s;
-	int i, j, ret;
-	int cnt = 0;
-	void *file_buf;
-	__u32 in_len, sz;
-	float tc = 0;
-	float speed;
-	ulong dstlen = 0;
+	struct timeval start_tval, end_tval;
+	int thread_num = opts->thread_num;
+	struct hizip_test_info info = {0};
+	struct wd_sched *sched = NULL;
+	int mode = opts->sync_mode;
+	__u64 src_len, dst_len, sz;
 	float total_in = 0;
 	float total_out = 0;
+	void *file_buf;
 	int count = 0;
-	struct wd_sched *sched = NULL;
-	struct hizip_test_info info = {0};
-	struct timeval start_tval, end_tval;
-	int mode = opts->sync_mode;
-	int thread_num = opts->thread_num;
+	struct stat s;
+	int i, j, ret;
+	float tc = 0;
+	float speed;
+	int fd;
 
 	fd = fileno(source);
 	SYS_ERR_COND(fstat(fd, &s) < 0, "fstat");
-	in_len = s.st_size;
+	src_len = s.st_size;
 
-	file_buf = calloc(1, in_len);
+	file_buf = calloc(1, src_len);
 
-	sz = fread(file_buf, 1, in_len, source);
-	if (sz != in_len)
-		WD_ERR("read file sz != in_len!\n");
-	count = in_len/block_size;
+	sz = fread(file_buf, 1, src_len, source);
+	if (sz != src_len)
+		WD_ERR("read file sz != src_len!\n");
 
+	count = src_len/block_size;
 	if (!count)
 		count = 1;
 
-	dbg("%s entry blocksize=%d, count=%d, threadnum= %d, in_len=%d\n",
-	    __func__, block_size, count, thread_num, in_len);
+	dbg("%s entry blocksize=%d, count=%d, threadnum= %d, src_len=%d\n",
+	    __func__, block_size, count, thread_num, src_len);
 
 	info.list = get_dev_list(opts, 1);
 	if (!info.list)
@@ -402,48 +526,50 @@ int comp_file_test(FILE *source, FILE *dest, struct test_options *opts)
 	if (ret)
 		goto out;
 
-	dstlen = (ulong)in_len * 2;
+	dst_len = (ulong)src_len * 10;
 	if (opts->block_size != 512000)
-		dstlen = opts->block_size; /* just for user configure dest size test*/
+		dst_len = opts->block_size; /* just for user configure dest size test*/
 
-	dstlen = dstlen > MAX_LEN ? MAX_LEN : dstlen;
+	dst_len = dst_len > MAX_LEN ? MAX_LEN : dst_len;
 
-	cnt = opts->thread_num;
-	for (i = 0; i < cnt; i++) {
-		test_thrds_data[i].thread_num = cnt;
+	for (i = 0; i < thread_num; i++) {
+		test_thrds_data[i].thread_num = thread_num;
 		test_thrds_data[i].cpu_id = i;
 		test_thrds_data[i].alg_type = opts->alg_type;
 		test_thrds_data[i].op_type = opts->op_type;
 		test_thrds_data[i].blksize = opts->block_size;
 		test_thrds_data[i].iteration = opts->run_num;
+		test_thrds_data[i].data_fmt = opts->data_fmt;
 		test_thrds_data[i].src = file_buf;
-		test_thrds_data[i].src_len = in_len;
-		test_thrds_data[i].dst_len = dstlen;
+		test_thrds_data[i].src_len = src_len;
+		test_thrds_data[i].dst_len = dst_len;
 		test_thrds_data[i].src = calloc(1, test_thrds_data[i].src_len);
 		if (test_thrds_data[i].src == NULL)
 			goto out_src;
-		memcpy(test_thrds_data[i].src, file_buf, in_len);
+		memcpy(test_thrds_data[i].src, file_buf, src_len);
 		test_thrds_data[i].dst = calloc(1, test_thrds_data[i].dst_len);
 		if (test_thrds_data[i].dst == NULL)
 			goto out_dst;
+		if (opts->alg_type == WD_LZ77_ZSTD)
+			test_thrds_data[i].priv = calloc(1,
+				sizeof(struct wd_lz77_zstd_data));
 	}
 
 	gettimeofday(&start_tval, NULL);
-	for (i = 0; i < cnt; i++) {
-		if (mode == CTX_MODE_ASYNC) {
+
+	for (i = 0; i < thread_num; i++) {
+		if (mode == CTX_MODE_ASYNC)
 			ret = pthread_create(&system_test_thrds[i], NULL,
 					     zip_sys_async_test_thread,
 					     &test_thrds_data[i]);
-		} else {
-			if (opts->is_stream == MODE_STREAM)
-				ret = pthread_create(&system_test_thrds[i], NULL,
-						     zlib_sys_stream_test_thread,
-						     &test_thrds_data[i]);
-			else
-				ret = pthread_create(&system_test_thrds[i], NULL,
-						     zip_sys_block_test_thread,
-						     &test_thrds_data[i]);
-		}
+		else if (opts->is_stream == MODE_STREAM)
+			ret = pthread_create(&system_test_thrds[i], NULL,
+					     zlib_sys_stream_test_thread,
+					     &test_thrds_data[i]);
+		else
+			ret = pthread_create(&system_test_thrds[i], NULL,
+					     zip_sys_block_test_thread,
+					     &test_thrds_data[i]);
 		if (ret) {
 			WD_ERR("Create %dth thread fail!\n", i);
 			goto out_thr;
@@ -451,11 +577,11 @@ int comp_file_test(FILE *source, FILE *dest, struct test_options *opts)
 	}
 
 	if (mode == CTX_MODE_ASYNC)
-			ret = pthread_create(&system_test_poll_thrds, NULL,
-					     zip_sys_async_test_poll_thread,
-					     &test_thrds_data[0]);
+		ret = pthread_create(&system_test_poll_thrds, NULL,
+				     zip_sys_async_test_poll_thread,
+				     &test_thrds_data[0]);
 
-	for (i = 0; i < opts->thread_num; i++) {
+	for (i = 0; i < thread_num; i++) {
 		ret = pthread_join(system_test_thrds[i], NULL);
 		if (ret) {
 			WD_ERR("Join %dth thread fail!\n", i);
@@ -472,24 +598,29 @@ int comp_file_test(FILE *source, FILE *dest, struct test_options *opts)
 	}
 
 	gettimeofday(&end_tval, NULL);
-	for (i = 0; i < opts->thread_num; i++) {
+
+	for (i = 0; i < thread_num; i++) {
 		total_in += test_thrds_data[i].src_len;
 		total_out += test_thrds_data[i].dst_len;
 	}
+
 	tc = (float)((end_tval.tv_sec-start_tval.tv_sec) * 1000000 +
 		     end_tval.tv_usec - start_tval.tv_usec);
 	dbg("%s end threadnum= %d, out_len=%u\n",
 	    __func__, thread_num, test_thrds_data[thread_num-1].dst_len);
 
 
-	if (mode == CTX_MODE_ASYNC) {
+	if (opts->alg_type == WD_LZ77_ZSTD)
+		sz = write_zstd_file(test_thrds_data[thread_num-1].priv,
+				     opts->data_fmt);
+	else if (mode == CTX_MODE_ASYNC)
 		sz = fwrite(test_thrds_data[thread_num - 1].dst, 1,
 			    out_len, dest);
 
-	} else {
+	else
 		sz = fwrite(test_thrds_data[thread_num - 1].dst, 1,
 			    test_thrds_data[thread_num - 1].dst_len, dest);
-	}
+
 	for (i = 0; i < thread_num; i++) {
 		dbg("%s:free src[%d]:%p\n", __func__, i, test_thrds_data[i].src);
 
@@ -538,4 +669,3 @@ out:
 
 	return ret;
 }
-
