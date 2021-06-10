ba8120b624df98f1db785e5b248298aa08541014
Yang Shen <shenyang39@huawei.com>
2021-06-10 20:21:28 +0800

    uadk: wd_comp - some static check fixes
    Fix some static check:
    1.fix typo.
    2.fix comments on the right.
    3.remove 'extern'.
    
    Signed-off-by: Yang Shen <youngershen@foxmail.com>

--- include/drv/wd_comp_drv.h
+++ include/drv/wd_comp_drv.h
@@ -18,19 +18,32 @@ enum wd_comp_state {
 /* fixme wd_comp_msg */
 struct wd_comp_msg {
 	struct wd_comp_req req;
-	__u32 tag;   	 /* request identifier */
-	__u8 alg_type;   /* Denoted by enum wcrypto_comp_alg_type */
-	__u8 comp_lv;    /* Denoted by enum wcrypto_comp_level */
-	__u8 stream_mode;/* Denoted by enum wcrypto_comp_state */
-	__u8 stream_pos; /* Denoted by enum wcrypto_stream_status */
-	__u16 data_fmt;   /* Data format, denoted by enum wd_buff_type */
-	__u16 win_sz;     /* Denoted by enum wcrypto_comp_win_type */
-	__u32 avail_out; /* Output buffer size */
-	__u32 in_cons;   /* consumed bytes of input data */
-	__u32 produced;  /* produced bytes of current operation */
-	__u32 isize;	 /* Denoted by gzip isize */
-	__u32 checksum;  /* Denoted by zlib/gzip CRC */
-	void *ctx_buf;   /* Denoted HW ctx cache, for stream mode */
+	/* request identifier */
+	__u32 tag;
+	/* Denoted by enum wcrypto_comp_alg_type */
+	__u8 alg_type;
+	/* Denoted by enum wcrypto_comp_level */
+	__u8 comp_lv;
+	/* Denoted by enum wcrypto_comp_state */
+	__u8 stream_mode;
+	/* Denoted by enum wcrypto_stream_status */
+	__u8 stream_pos;
+	/* Data format, denoted by enum wd_buff_type */
+	__u16 data_fmt;
+	/* Denoted by enum wcrypto_comp_win_type */
+	__u16 win_sz;
+	/* Output buffer size */
+	__u32 avail_out;
+	/* consumed bytes of input data */
+	__u32 in_cons;
+	/* produced bytes of current operation */
+	__u32 produced;
+	/* Denoted by gzip isize */
+	__u32 isize;
+	/* Denoted by zlib/gzip CRC */
+	__u32 checksum;
+	/* Denoted HW ctx cache, for stream mode */
+	void *ctx_buf;
 };
 
 struct wd_comp_driver {
