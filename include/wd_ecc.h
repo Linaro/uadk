4755587bf05a957661071e40cda01e3bdf4e7f52
Wenkai Lin <linwenkai6@hisilicon.com>
2021-08-02 16:01:20 +0800

    wd: env: optimize for wd parse env
    1.reduce wd env variable to WD_<alg>_CTX_NUM
    2.add maintenance and test interface
    
    Signed-off-by: Wenkai Lin <linwenkai6@hisilicon.com>

--- include/wd_ecc.h
+++ include/wd_ecc.h
@@ -493,6 +493,33 @@ int wd_ecc_env_init(void);
  */
 void wd_ecc_env_uninit(void);
 
+/**
+ * wd_ecc_ctx_num_init() - request ctx for ecc.
+ * @node:       numa node id.
+ * @type:       operation type.
+ * @num:        ctx number.
+ * @mode:       0: sync mode, 1: async mode
+ */
+int wd_ecc_ctx_num_init(__u32 node, __u32 type, __u32 num, __u8 mode);
+
+/**
+ * wd_ecc_ctx_num_uninit() - UnInit ctx and schedule resources
+ * set by above init.
+ *
+ */
+void wd_ecc_ctx_num_uninit(void);
+
+/**
+ * wd_ecc_get_env_param() - query the number of CTXs that meet input attributes.
+ * @node:       numa node id.
+ * @type:       operation type.
+ * @mode:       0: sync mode, 1: async mode
+ * @num:        return ctx num.
+ * @is_enable   return enable inner poll flag.
+ */
+int wd_ecc_get_env_param(__u32 node, __u32 type, __u32 mode,
+			 __u32 *num, __u8 *is_enable);
+
 #ifdef __cplusplus
 }
 #endif
