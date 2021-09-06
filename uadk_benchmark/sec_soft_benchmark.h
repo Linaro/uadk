c48c69bd49c4f23c09f3b810075c9b649970c0a5
Liulongfang <liulongfang@huawei.com>
2021-09-06 13:57:45 +0800

    uadk/benchmark: add uadk perf test tools
    Add a comprehensive performance test tool to the current uadk module
    to uniformly test the performance of sva, nosva, openssl and openssl
    engine modes.
    
    Signed-off-by: Longfang Liu <liulongfang@huawei.com>

---
+++ uadk_benchmark/sec_soft_benchmark.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+#ifndef SEC_SOFT_BENCHMARK_H
+#define SEC_SOFT_BENCHMARK_H
+
+#include "uadk_benchmark.h"
+
+
+extern int sec_soft_benchmark(struct acc_option *options);
+#endif /* SEC_SOFT_BENCHMARK_H */
