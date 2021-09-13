b74e031df9fb054df83ecfe75a7e95e3f7ac7cab
Wenkai Lin <linwenkai6@hisilicon.com>
2021-09-13 15:40:53 +0800

    wd: fix wd ctx mmap
    mmap return MAP_FAILED instead of null.
    
    Signed-off-by: Wenkai Lin <linwenkai6@hisilicon.com>

--- wd.c
+++ wd.c
@@ -369,7 +369,7 @@ void *wd_ctx_mmap_qfr(handle_t h_ctx, enum uacce_qfrt qfrt)
 	size = ctx->qfrs_offs[qfrt];
 
 	addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, ctx->fd, off);
-	if (!addr)
+	if (addr == MAP_FAILED)
 		return NULL;
 
 	ctx->qfrs_base[qfrt] = addr;
