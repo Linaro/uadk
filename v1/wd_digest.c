026ab4f61d5ea96b55594815d26947c70aa534e0
Kai Ye <yekai13@huawei.com>
2021-12-14 19:24:48 +0800

    digest/v1: fixed hmac key 0 length checking
    The length of keylen checking v1 and v2 should be the same.
    
    Signed-off-by: Kai Ye <yekai13@huawei.com>

--- v1/wd_digest.c
+++ v1/wd_digest.c
@@ -243,7 +243,7 @@ int wcrypto_set_digest_key(void *ctx, __u8 *key, __u16 key_len)
 		return -WD_EINVAL;
 	}
 
-	if (key_len > MAX_HMAC_KEY_SIZE) {
+	if (key_len == 0 || key_len > MAX_HMAC_KEY_SIZE) {
 		WD_ERR("%s: input key length err!\n", __func__);
 		return -WD_EINVAL;
 	}
