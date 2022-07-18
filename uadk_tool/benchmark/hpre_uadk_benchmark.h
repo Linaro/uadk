c6fef630c6e31dc6ede5737a7f46f297f5bc7991
Longfang Liu <liulongfang@huawei.com>
2022-07-18 17:16:32 +0800

    uadk_tool: Add SVA test function of HPRE algorithm
    Add the performance test function of SVA mode of
    RSA, DH, ECC algorithms for HPRE module.
    
    Signed-off-by: Longfang Liu <liulongfang@huawei.com>

---
+++ uadk_tool/benchmark/hpre_uadk_benchmark.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+#ifndef HPRE_UADK_BENCHMARK_H
+#define HPRE_UADK_BENCHMARK_H
+
+extern int hpre_uadk_benchmark(struct acc_option *options);
+#endif /* HPRE_UADK_BENCHMARK_H */
