2485ddcebb70a96694da3fde1ea5de222053a685
Wenkai Lin <linwenkai6@hisilicon.com>
2022-07-23 16:50:25 +0800

    uadk: fix pointer parameters are not checked
    Pointer parameters of the "wd_xxx_poll" function are not checked
    whether the pointer is null. Param: "count".
    
    Signed-off-by: Wenkai Lin <linwenkai6@hisilicon.com>

--- wd_ecc.c
+++ wd_ecc.c
@@ -2176,6 +2176,11 @@ int wd_ecc_poll(__u32 expt, __u32 *count)
 {
 	handle_t h_sched_sess = wd_ecc_setting.sched.h_sched_ctx;
 
+	if (unlikely(!count)) {
+		WD_ERR("invalid: ecc poll param count is NULL!\n");
+		return -WD_EINVAL;
+	}
+
 	return wd_ecc_setting.sched.poll_policy(h_sched_sess, expt, count);
 }
 
