2be9bf5f67503352c7e7429cf9889e892f0620e4
Wenkai Lin <linwenkai6@hisilicon.com>
2022-07-23 16:50:29 +0800

    uadk: fix macro argument warning
    macro argument 'array' should be enclosed in parentheses.
    
    Signed-off-by: Wenkai Lin <linwenkai6@hisilicon.com>

--- include/wd_util.h
+++ include/wd_util.h
@@ -18,8 +18,8 @@ extern "C" {
 #endif
 
 #define FOREACH_NUMA(i, config, config_numa) \
-	for (i = 0, config_numa = config->config_per_numa; \
-	     i < config->numa_num; config_numa++, i++)
+	for ((i) = 0, (config_numa) = (config)->config_per_numa; \
+	     (i) < (config)->numa_num; (config_numa)++, (i)++)
 
 struct wd_async_msg_pool {
 	struct msg_pool *pools;
