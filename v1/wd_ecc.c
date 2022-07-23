27d2778c9e55ef482ac01d265061344e7742f01a
Wenkai Lin <linwenkai6@hisilicon.com>
2022-07-23 16:50:32 +0800

    uadk: fix function params usage
    parameter 'expt' of function 'wd_rsa_poll_ctx'
    should not be used as working variable.
    
    Signed-off-by: Wenkai Lin <linwenkai6@hisilicon.com>

--- v1/wd_ecc.c
+++ v1/wd_ecc.c
@@ -269,14 +269,15 @@ static void init_dtb_param(void *dtb, char *start,
 			   __u32 dsz, __u32 bsz, __u32 num)
 {
 	struct wd_dtb *tmp = dtb;
+	char *pos = start;
 	int i = 0;
 
 	while (i++ < num) {
-		tmp->data = start;
+		tmp->data = pos;
 		tmp->dsize = dsz;
 		tmp->bsize = bsz;
 		tmp += 1;
-		start += bsz;
+		pos += bsz;
 	}
 }
 
