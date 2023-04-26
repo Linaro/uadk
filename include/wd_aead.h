d2c6f1740d23473355b12a477b368ae3e49d62bd
Hao Fang <fanghao11@huawei.com>
2023-04-26 11:55:26 +0800

    uadk/aead: introduce the init2 interface for aead
    The basic init process is complex for users who need to
    know the device, scheduler, etc.
    
    So introduce the init2 interface just for simplifying
    the initialization process when user use the aead algorithm.
    
    Signed-off-by: Hao Fang <fanghao11@huawei.com>

--- include/wd_aead.h
+++ include/wd_aead.h
@@ -97,6 +97,34 @@ int wd_aead_init(struct wd_ctx_config *config, struct wd_sched *sched);
 void wd_aead_uninit(void);
 
 /**
+ * wd_aead_init2_() - A simplify interface to initializate uadk
+ * aead operation. This interface keeps most functions of
+ * wd_aead_init(). Users just need to descripe the deployment of
+ * business scenarios. Then the initialization will request appropriate
+ * resources to support the business scenarios.
+ * To make the initializate simpler, ctx_params support set NULL.
+ * And then the function will set them as driver's default.
+ * Please do not use this interface with wd_aead_init() together, or
+ * some resources may be leak.
+ *
+ * @alg: The algorithm users want to use.
+ * @sched_type: The scheduling type users want to use.
+ * @task_type: Task types, including soft computing, hardware and hybrid computing.
+ * @ctx_params: The ctxs resources users want to use. Include per operation
+ * type ctx numbers and business process run numa.
+ *
+ * Return 0 if succeed and others if fail.
+ */
+int wd_aead_init2_(char *alg, __u32 sched_type, int task_type, struct wd_ctx_params *ctx_params);
+
+#define wd_aead_init2(alg, sched_type, task_type) \
+	wd_aead_init2_(alg, sched_type, task_type, NULL)
+
+/**
+ * wd_aead_uninit2() - Uninitialise ctx configuration and scheduler.
+ */
+void wd_aead_uninit2(void);
+/**
  * wd_aead_alloc_sess() Allocate a wd aead session
  * @ setup Parameters to setup this session.
  */
