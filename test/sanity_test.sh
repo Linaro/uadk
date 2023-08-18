b9b90fd15c4d6de41e3501560ced2452cc1e9376
Wenkai Lin <linwenkai6@hisilicon.com>
2023-08-18 10:31:29 +0800

    uadk: fix sec test
    Fix for uadk sec test code moved to uadk_tool
    
    Signed-off-by: Wenkai Lin <linwenkai6@hisilicon.com>

--- test/sanity_test.sh
+++ test/sanity_test.sh
@@ -374,16 +374,16 @@ run_zip_test_v2()
 # failed: return 1; success: return 0
 run_sec_test_v2()
 {
-	run_cmd test_hisi_sec --cipher 0 --optype 0 --pktlen 16 --keylen 16 \
+	run_cmd uadk_tool test --m sec --cipher 0 --optype 0 --pktlen 16 --keylen 16 \
 		--times 1 --sync --multi 1 $@
 
-	run_cmd test_hisi_sec --cipher 0 --optype 0 --pktlen 16 --keylen 16 \
+	run_cmd uadk_tool test --m sec --cipher 0 --optype 0 --pktlen 16 --keylen 16 \
 		--times 1 --async --multi 1 $@
 
-	run_cmd test_hisi_sec --digest 0 --optype 0 --pktlen 16 --keylen 16 \
+	run_cmd uadk_tool test --m sec --digest 0 --optype 0 --pktlen 16 --keylen 16 \
 		--times 1 --sync --multi 1 $@
 
-	run_cmd test_hisi_sec --digest 0 --optype 0 --pktlen 16 --keylen 16 \
+	run_cmd uadk_tool test --m sec test_hisi_sec --digest 0 --optype 0 --pktlen 16 --keylen 16 \
 		--times 1 --async --multi 1 $@
 }
 
