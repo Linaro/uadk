e018308c64d35b73779298ffb361031a30a9426e
Weili Qian <qianweili@huawei.com>
2023-11-10 11:52:24 +0800

    uadk_tool: Support performance test for TRNG.
    Support performance test for TRNG.
    
    Signed-off-by: Weili Qian <qianweili@huawei.com>

---
+++ uadk_tool/benchmark/trng_wd_benchmark.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef TRNG_WD_BENCHMARK_H
+#define TRNG_WD_BENCHMARK_H
+
+extern int trng_wd_benchmark(struct acc_option *options);
+#endif /* TRNG_WD_BENCHMARK_H */
