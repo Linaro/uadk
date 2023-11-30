cdd48cc72a53a527e3a570215722337ac54e663c
Longfang Liu <liulongfang@huawei.com>
2023-11-30 17:02:26 +0800

    uadk: fixed some input parameter checking issues
    Inside the asynchronous packet receiving interface of all submodules.
    When the number of expected received packets is 0. Failure to intercept
    will result in abnormal packet recycling.
    As a result, the number of normal service packets does not match.
    
    In addition, when the expected value is 0, it will cause the while loop
    control adjustment to flip, resulting in other receiving operations.
    
    Signed-off-by: Longfang Liu <liulongfang@huawei.com>

--- wd_rsa.c
+++ wd_rsa.c
@@ -495,8 +495,8 @@ int wd_rsa_poll_ctx(__u32 idx, __u32 expt, __u32 *count)
 	__u32 tmp = expt;
 	int ret;
 
-	if (unlikely(!count)) {
-		WD_ERR("invalid: param count is NULL!\n");
+	if (unlikely(!count || !expt)) {
+		WD_ERR("invalid: rsa poll count or expt is NULL!\n");
 		return -WD_EINVAL;
 	}
 
