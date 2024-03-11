34c49db7d9eba5255f678179da95a15976dbb305
Chenghai Huang <huangchenghai2@huawei.com>
2024-03-11 16:36:13 +0800

    uadk tool: fix the msg pool release bug of async zip benchmark
    Ensure that all packets in the msg pool are removed before end.
    In V2, resources such as tags are released in a centralized manner
    to avoid errors caused by asynchronous resource release sequence.
    In V1, before the packet sending thread releases the tag, ensure
    that the poll thread has ended.
    
    Signed-off-by: Chenghai Huang <huangchenghai2@huawei.com>

--- uadk_tool/benchmark/uadk_benchmark.c
+++ uadk_tool/benchmark/uadk_benchmark.c
@@ -594,6 +594,7 @@ static int benchmark_run(struct acc_option *option)
 		} else if (option->modetype == NOSVA_MODE) {
 			ret = zip_wd_benchmark(option);
 		}
+		break;
 	case TRNG_TYPE:
 		if (option->modetype == SVA_MODE)
 			ACC_TST_PRT("TRNG not support sva mode..\n");
@@ -727,7 +728,8 @@ static void print_help(void)
 	ACC_TST_PRT("    [--mode sva/nosva/soft/sva-soft/nosva-soft/instr/multibuff]: start UADK or Warpdrive or Openssl or Instruction mode test\n");
 	ACC_TST_PRT("    [--sync/--async]: start asynchronous/synchronous mode test\n");
 	ACC_TST_PRT("    [--opt 0,1,2,3,4,5]:\n");
-	ACC_TST_PRT("        SEC/ZIP: 0/1:encryption/decryption or compression/decompression\n");
+	ACC_TST_PRT("        SEC: cipher,aead: 0/1:encryption/decryption; digest: 0/1:normal/hmac\n");
+	ACC_TST_PRT("        ZIP: 0~1:block compression, block decompression; 2~3:stream compression, stream decompression\n");
 	ACC_TST_PRT("        HPRE: 0~5:keygen, key compute, Enc, Dec, Sign, Verify\n");
 	ACC_TST_PRT("    [--pktlen]:\n");
 	ACC_TST_PRT("        set the length of BD message in bytes\n");
