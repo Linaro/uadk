17e5f25df480a5cacc3ac5e8ae88b708786eec44
Chenghai Huang <huangchenghai2@huawei.com>
2024-03-11 16:22:58 +0800

    uadk/tools - support the nosva test of a specified device
    Add the description of device parameters. The input should
    use the device name from '/sys/class/uacce/<name>'. Only full
    matching device names are supported.
    
    Signed-off-by: Chenghai Huang <huangchenghai2@huawei.com>

--- uadk_tool/benchmark/sec_wd_benchmark.c
+++ uadk_tool/benchmark/sec_wd_benchmark.c
@@ -600,6 +600,14 @@ static int init_wd_queue(struct acc_option *options)
 		/* nodemask need to    be clean */
 		g_thread_queue.bd_res[i].queue->node_mask = 0x0;
 		memset(g_thread_queue.bd_res[i].queue->dev_path, 0x0, PATH_STR_SIZE);
+		if (strlen(options->device) != 0) {
+			ret = snprintf(g_thread_queue.bd_res[i].queue->dev_path,
+					PATH_STR_SIZE, "%s", options->device);
+			if (ret < 0) {
+				WD_ERR("failed to copy dev file path!\n");
+				return -WD_EINVAL;
+			}
+		}
 
 		ret = wd_request_queue(g_thread_queue.bd_res[i].queue);
 		if (ret) {
