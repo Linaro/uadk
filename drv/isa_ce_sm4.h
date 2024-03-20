d83048f61bfb130562c0cec25de09844e9f556af
Yang Shen <shenyang39@huawei.com>
2024-03-20 16:15:00 +0800

    uadk/cipher: isa_ce - support SM4 cbc_cts mode
    This patch implements the CE instruction using SM4 CBC_CTS modes.
    
    Signed-off-by: Yang Shen <shenyang39@huawei.com>

--- drv/isa_ce_sm4.h
+++ drv/isa_ce_sm4.h
@@ -25,27 +25,35 @@ struct sm4_ce_drv_ctx {
 
 void sm4_v8_set_encrypt_key(const unsigned char *userKey, struct SM4_KEY *key);
 void sm4_v8_set_decrypt_key(const unsigned char *userKey, struct SM4_KEY *key);
+
 void sm4_v8_cbc_encrypt(const unsigned char *in, unsigned char *out,
 			size_t length, const struct SM4_KEY *key,
 			unsigned char *ivec, const int enc);
+void sm4_v8_cbc_cts_encrypt(const unsigned char *in, unsigned char *out,
+			    size_t len, const void *key, const unsigned char ivec[16]);
+void sm4_v8_cbc_cts_decrypt(const unsigned char *in, unsigned char *out,
+			    size_t len, const void *key, const unsigned char ivec[16]);
+
 void sm4_v8_ecb_encrypt(const unsigned char *in, unsigned char *out,
 			size_t length, const struct SM4_KEY *key, const int enc);
+
 void sm4_v8_ctr32_encrypt_blocks(const unsigned char *in, unsigned char *out,
-			size_t len, const void *key, const unsigned char ivec[16]);
+				 size_t len, const void *key, const unsigned char ivec[16]);
 
 void sm4_v8_cfb_encrypt_blocks(const unsigned char *in, unsigned char *out,
-		       size_t length, const struct SM4_KEY *key, unsigned char *ivec);
+			       size_t length, const struct SM4_KEY *key, unsigned char *ivec);
 void sm4_v8_cfb_decrypt_blocks(const unsigned char *in, unsigned char *out,
-		       size_t length, const struct SM4_KEY *key, unsigned char *ivec);
+			       size_t length, const struct SM4_KEY *key, unsigned char *ivec);
+
 void sm4_v8_crypt_block(const unsigned char *in, unsigned char *out,
-		       const struct SM4_KEY *key);
+			const struct SM4_KEY *key);
 
 int sm4_v8_xts_encrypt(const unsigned char *in, unsigned char *out, size_t length,
-				const struct SM4_KEY *key, unsigned char *ivec,
-				const struct SM4_KEY *key2);
+		       const struct SM4_KEY *key, unsigned char *ivec,
+		       const struct SM4_KEY *key2);
 int sm4_v8_xts_decrypt(const unsigned char *in, unsigned char *out, size_t length,
-				const struct SM4_KEY *key, unsigned char *ivec,
-				const struct SM4_KEY *key2);
+		       const struct SM4_KEY *key, unsigned char *ivec,
+		       const struct SM4_KEY *key2);
 
 #ifdef __cplusplus
 }
