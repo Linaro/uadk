afc1082d85c6cdac7d3eaaac8130ffb5fa39b138
Weili Qian <qianweili@huawei.com>
2024-05-31 17:27:38 +0800

    uadk/v1: check queue status before sending doorbells
    When the device needs to be reset, the queue status is
    set to disable before resetting. The user process checks
    the queue status before sending the doorbell. If the queue
    is disable, the user process returns failure.
    
    Currently, the task execution order in user mode is as follows:
    1. check the queue status.
    2. fill in or parse the BD.
    3. send the doorbell to the hardware.
    
    To reduce the possibility of sending doorbells during reset,
    the task execution order is modified as follows:
    1. fill in or parse the BD.
    2. check the queue status.
    3. send the doorbell to the hardware.
    
    In addition, a rmb() is added before the doorbell is
    sent to ensure that the queue status check is complete.
    
    rmb() and wmb() can be replaced by mb() in hisi_qm_send().
    Therefore, the barrier on the wd_reg_read() and wd_reg_write()
    can be deleted.
    
    Signed-off-by: Weili Qian <qianweili@huawei.com>
    Signed-off-by: Qi Tao <taoqi10@huawei.com>

--- v1/drv/hisi_hpre_udrv.c
+++ v1/drv/hisi_hpre_udrv.c
@@ -1938,10 +1938,10 @@ static int fill_sm2_enc_sqe(void *msg, struct qm_queue_info *info, __u16 idx)
 		goto fail_fill_sqe;
 	}
 
-	/* make sure the request is all in memory before doorbell */
-	mb();
 	info->sq_tail_index = i;
-	qm_tx_update(info, 1);
+	ret = qm_tx_update(info, 1);
+	if (unlikely(ret))
+		goto fail_fill_sqe;
 
 	return ret;
 
@@ -2091,7 +2091,9 @@ static int parse_first_sqe(void *hw_msg, struct qm_queue_info *info, __u16 idx,
 		WD_ERR("first BD error = %u\n", msg->result);
 
 	info->cq_head_index = i;
-	qm_rx_update(info, 1);
+	ret = qm_rx_update(info, 1);
+	if (unlikely(ret))
+		return ret;
 
 	return 1;
 }
