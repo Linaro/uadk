ee0686b8b9738b3c755e35873f3af9cd4d69b603
Chenghai Huang <huangchenghai2@huawei.com>
2024-05-31 17:23:02 +0800

    uadk - modify error report information of pool management functions
    The wd_find_msg_in_pool function and the wd_get_msg_from_pool
    function report the same error information, which affects code
    search during fault locating.
    
    Signed-off-by: Chenghai Huang <huangchenghai2@huawei.com>
    Signed-off-by: Qi Tao <taoqi10@huawei.com>

--- wd_rsa.c
+++ wd_rsa.c
@@ -553,7 +553,7 @@ int wd_rsa_poll_ctx(__u32 idx, __u32 expt, __u32 *count)
 		msg = wd_find_msg_in_pool(&wd_rsa_setting.pool, idx,
 					  recv_msg.tag);
 		if (!msg) {
-			WD_ERR("failed to get msg from pool!\n");
+			WD_ERR("failed to find msg from pool!\n");
 			return -WD_EINVAL;
 		}
 
