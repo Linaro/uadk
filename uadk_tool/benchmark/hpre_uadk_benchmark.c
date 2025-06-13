039bc5bbdabbbaf17ffcc3e6655a1a3b50864915
lizhi <lizhi206@huawei.com>
2025-06-13 15:01:21 +0800

    uadk_tool: adapt ecdh benchmark with secp256r1 curve
    1. Add ecdh benchmark with secp256r1 curve
    2. To diff with original mode, the new mode
    is named ecdh-256r1, adding 'r1' as suffix.
    
    Signed-off-by: lizhi <lizhi206@huawei.com>
    Signed-off-by: Qi Tao <taoqi10@huawei.com>

--- uadk_tool/benchmark/hpre_uadk_benchmark.c
+++ uadk_tool/benchmark/hpre_uadk_benchmark.c
@@ -11,10 +11,12 @@
 #include "include/wd_ecc.h"
 #include "include/wd_sched.h"
 
-#define ECC_CURVE_ID		0x3 /* def set secp256k1 */
+#define ECC_CURVE_SECP256K1	0x3 /* def set with secp256k1 */
+#define ECC_CURVE_SECP256R1	0x7 /* optional set with secp256r1 */
 #define HPRE_TST_PRT 		printf
 #define ERR_OPTYPE		0xFF
 #define SM2_DG_SZ		1024
+#define WD_SECP256R1		0x18 /* consistent with wd_ecc.c */
 
 struct hpre_rsa_key_in {
 	void *e;
@@ -92,6 +94,7 @@ typedef struct uadk_thread_res {
 	u32 kmode;
 	u32 optype;
 	u32 td_id;
+	u32 algtype;
 } thread_data;
 
 static struct wd_ctx_config g_ctx_cfg;
@@ -217,6 +220,9 @@ static void get_ecc_param(u32 algtype, u32 *keysize)
 	case ECDH_256:
 		*keysize = 256;
 		break;
+	case ECDH_256R1:
+		*keysize = 256;
+		break;
 	case ECDH_384:
 		*keysize = 384;
 		break;
@@ -337,6 +343,7 @@ static int hpre_uadk_param_parse(thread_data *tddata, struct acc_option *options
 	tddata->keybits = keysize;
 	tddata->kmode = mode;
 	tddata->optype = optype;
+	tddata->algtype = algtype;
 
 	HPRE_TST_PRT("%s to run %s task!\n", options->algclass,
 			alg_operations[options->optype]);
@@ -1037,6 +1044,10 @@ static int get_ecc_curve(struct hpre_ecc_setup *setup, u32 cid)
 	case 6: // secp521R1
 		setup->nid = 716;
 		setup->curve_id = WD_SECP521R1;
+		break;
+	case 7: // secp256R1
+		setup->nid = 415;
+		setup->curve_id = WD_SECP256R1;
 		break;
 	default:
 		HPRE_TST_PRT("failed to get ecc curve id!\n");
@@ -2253,7 +2264,7 @@ static void *ecc_uadk_sync_run(void *arg)
 	struct wd_ecc_point pbk;
 	struct wd_dtb prk;
 	struct wd_ecc_req req;
-	u32 cid = ECC_CURVE_ID;
+	u32 cid;
 	handle_t h_sess;
 	u32 count = 0;
 	int ret;
@@ -2263,6 +2274,12 @@ static void *ecc_uadk_sync_run(void *arg)
 	memset(&req,     0, sizeof(req));
 
 	memset(&setup,     0, sizeof(setup));
+
+	if (pdata->algtype == ECDH_256R1)
+		cid = ECC_CURVE_SECP256R1;
+	else
+		cid = ECC_CURVE_SECP256K1;
+
 	if (subtype != X448_TYPE && subtype != X25519_TYPE) {
 		ret = get_ecc_curve(&setup, cid);
 		if (ret)
@@ -2271,7 +2288,7 @@ static void *ecc_uadk_sync_run(void *arg)
 
 	sess_setup.key_bits = pdata->keybits;
 	if (subtype == ECDH_TYPE || subtype == ECDSA_TYPE) {
-		if (cid > ECC_CURVE_ID) {
+		if (cid > ECC_CURVE_SECP256R1) {
 			sess_setup.cv.type = WD_CV_CFG_PARAM;
 			get_ecc_key_param(&param, pdata->keybits);
 			sess_setup.cv.cfg.pparam = &param;
@@ -2411,7 +2428,7 @@ static void *ecc_uadk_async_run(void *arg)
 	struct wd_ecc_point pbk;
 	struct wd_ecc_req req;
 	struct wd_dtb prk;
-	u32 cid = ECC_CURVE_ID;
+	u32 cid;
 	handle_t h_sess;
 	int try_cnt = 0;
 	u32 count = 0;
@@ -2422,6 +2439,12 @@ static void *ecc_uadk_async_run(void *arg)
 	memset(&req,	 0, sizeof(req));
 
 	memset(&setup,	   0, sizeof(setup));
+
+	if (pdata->algtype == ECDH_256R1)
+		cid = ECC_CURVE_SECP256R1;
+	else
+		cid = ECC_CURVE_SECP256K1;
+
 	if (subtype != X448_TYPE && subtype != X25519_TYPE) {
 		ret = get_ecc_curve(&setup, cid);
 		if (ret)
@@ -2430,7 +2453,7 @@ static void *ecc_uadk_async_run(void *arg)
 
 	sess_setup.key_bits = pdata->keybits;
 	if (subtype == ECDH_TYPE || subtype == ECDSA_TYPE) {
-		if (cid > ECC_CURVE_ID) {
+		if (cid > ECC_CURVE_SECP256R1) {
 			sess_setup.cv.type = WD_CV_CFG_PARAM;
 			get_ecc_key_param(&param, pdata->keybits);
 			sess_setup.cv.cfg.pparam = &param;
@@ -2627,6 +2650,7 @@ static int hpre_uadk_sync_threads(struct acc_option *options)
 		threads_args[i].keybits = threads_option.keybits;
 		threads_args[i].optype = threads_option.optype;
 		threads_args[i].td_id = i;
+		threads_args[i].algtype = threads_option.algtype;
 		ret = pthread_create(&tdid[i], NULL, uadk_hpre_sync_run, &threads_args[i]);
 		if (ret) {
 			HPRE_TST_PRT("Create sync thread fail!\n");
@@ -2703,6 +2727,7 @@ static int hpre_uadk_async_threads(struct acc_option *options)
 		threads_args[i].keybits = threads_option.keybits;
 		threads_args[i].optype = threads_option.optype;
 		threads_args[i].td_id = i;
+		threads_args[i].algtype = threads_option.algtype;
 		ret = pthread_create(&tdid[i], NULL, uadk_hpre_async_run, &threads_args[i]);
 		if (ret) {
 			HPRE_TST_PRT("Create async thread fail!\n");
