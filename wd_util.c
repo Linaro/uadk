406dc8a7c6301f78ad5027c741cd29bd65e60ac3
Qinxin Xia <xiaqinxin@huawei.com>
2025-08-07 19:41:10 +0800

    uadk: wd_comp - support the new algorithm 'lz4'
    Support the algorithm 'lz4' in wd_comp
    
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>
    Signed-off-by: Qi Tao <taoqi10@huawei.com>

--- wd_util.c
+++ wd_util.c
@@ -107,6 +107,7 @@ static struct acc_alg_item alg_options[] = {
 	{"gzip", "gzip"},
 	{"deflate", "deflate"},
 	{"lz77_zstd", "lz77_zstd"},
+	{"lz4", "lz4"},
 	{"lz77_only", "lz77_only"},
 	{"hashagg", "hashagg"},
 	{"udma", "udma"},
