e53a1a4988a483958bd00045aa9a846c7cc4b128
ZongYu Wu <wuzongyu1@huawei.com>
2025-09-27 12:04:10 +0800

    uadk: Convert signed to unsigned in uadk drv bitwise operations
    Convert signed to unsigned in uadk drv bitwise operations
    
    Signed-off-by: ZongYu Wu <wuzongyu1@huawei.com>

--- drv/hisi_comp_huf.c
+++ drv/hisi_comp_huf.c
@@ -76,7 +76,7 @@ static long read_bits(struct bit_reader *br, __u32 n)
 	if (br->cur_pos + n > br->total_bits)
 		return -WD_EINVAL;
 
-	ret = (br->data >> br->cur_pos) & ((1L << n) - 1L);
+	ret = (br->data >> br->cur_pos) & ((1UL << n) - 1UL);
 	br->cur_pos += n;
 
 	return ret;
@@ -85,7 +85,7 @@ static long read_bits(struct bit_reader *br, __u32 n)
 static int check_store_huffman_block(struct bit_reader *br)
 {
 	__u32 pad, bit_len;
-	long data;
+	unsigned long data;
 
 	bit_len = br->total_bits - br->cur_pos;
 
@@ -111,8 +111,8 @@ static int check_store_huffman_block(struct bit_reader *br)
 
 static int check_fix_huffman_block(struct bit_reader *br)
 {
-	long bit, len_idx, dist_code, extra;
-	long code, bits;
+	long bits, bit, len_idx, dist_code, extra;
+	unsigned long code, ubit;
 
 	while (br->cur_pos < br->total_bits) {
 		/* reads 7~9 bits to determine literal/length */
@@ -123,7 +123,8 @@ static int check_fix_huffman_block(struct bit_reader *br)
 			if (bit < 0)
 				return BLOCK_IS_INCOMPLETE;
 
-			code = (code << 1) | bit;
+			ubit = bit;
+			code = (code << 1) | ubit;
 			bits++;
 
 			/*
